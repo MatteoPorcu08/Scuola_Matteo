5c119ecfb65385b7185ad8fa20cd4cfa020fe04c
MatteoPorcu08 <porcumatteo@asproni-fermi.edu.it>
2025-09-26 09:17:51 +0200

    Commit automatico 26/09/2025  9:17:51,74

---
+++ Informatica/Programmazione/C/Scheda Smartphone.C
@@ -0,0 +1,46 @@
+#include <stdio.h>
+#include <string.h>
+
+struct Smartphone{
+    char modello[40];
+    char marca[30];
+    int memoriaGB;     // memoria interna in GB
+    int ramGB;         // RAM in GB
+    double peso;       // peso in grammi
+};
+
+int main(){
+    
+    Smartphone S1,S2;
+
+    //chiede di inserire i dati del primo telfono
+    printf("Inserisci i dati del primo telefono:");
+    printf("Che modello e' il tuo telefono?\n");
+    scanf("%s",S1.modello);
+    printf("Che marca e' il tuo telefono?\n");
+    scanf("%s",S1.marca);
+    printf("Quanta memoria ROM ha il tuo telefono?\n");
+    scanf("%d", &S1.memoriaGB);
+    printf("Quanta memoria RAM ha il tuo telefono?\n");
+    scanf("%d", &S1.ramGB);
+    printf("Quanto pesa il tuo telefono?\n");
+    scanf("%lf", &S1.peso);
+    stampaDati1(S1); //commando per stampare i dati del primo telefono
+
+    //chiede di inserire i dati del secondo telefono
+    printf("Inserisci i dati del secondo telefono:");
+    printf("Che modello e' il tuo telefono?\n");
+    scanf("%s",S2.modello);
+    printf("Che marca e' il tuo telefono?\n");
+    scanf("%s",S2.marca);
+    printf("Quanta memoria ROM ha il tuo telefono?\n");
+    scanf("%d", &S2.memoriaGB);
+    printf("Quanta memoria RAM ha il tuo telefono?\n");
+    scanf("%d", &S2.ramGB);
+    printf("Quanto pesa il tuo telefono?\n");
+    scanf("%lf", &S2.peso);
+    stampaDati2(S2); //commando per stampare i dati del secondo telefono
+
+
+
+}
