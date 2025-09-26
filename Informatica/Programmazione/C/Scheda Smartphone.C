4aeacad165970479087f852d1dea34f026f37bfe
MatteoPorcu08 <porcumatteo@asproni-fermi.edu.it>
2025-09-26 10:19:17 +0200

    Commit automatico 26/09/2025 10:19:17,05

--- Informatica/Programmazione/C/Scheda Smartphone.C
+++ Informatica/Programmazione/C/Scheda Smartphone.C
@@ -9,12 +9,14 @@ struct Smartphone{
     double peso;       // peso in grammi
 };
 
+void caricaSmartphone(Smartphone *s);
+
 void stampaSmartphone(Smartphone s);
 
 int main(){
     
     Smartphone S1,S2;
-
+    /*/
     //input i dati del primo telfono
     printf("Inserisci i dati del primo telefono:\n");
     printf("Che modello e' il tuo telefono?\n");
@@ -43,11 +45,13 @@ int main(){
     printf("Quanto pesa il tuo telefono?(g)\n");
     scanf("%lf", &S2.peso);
     printf("\n");
-
+    */
     //stampa dati inseriti
+    caricaSmartphone(&S1);
     printf("I dati  del primo smarthone sono:\n");
     stampaSmartphone(S1);
     printf("\n");
+    caricaSmartphone(&S2);
     printf("I dati del seocndo smartphone sono:\n");
     stampaSmartphone(S2);
 
@@ -62,3 +66,19 @@ void stampaSmartphone(Smartphone s){
         printf("Il telefono ha %d GB di memoria RAM\n",s.ramGB);
         printf("Il telefono pesa %.2lf g",s.peso);
     }
+
+//procedura caricaSmartphone
+void caricaSmartphone(Smartphone *S2){
+    printf("Inserisci i dati del secondo telefono:\n");
+    printf("Che modello e' il tuo telefono?\n");
+    scanf(" %[^\n]s",S2->modello);
+    printf("Che marca e' il tuo telefono?\n");
+    scanf(" %s",S2->marca);
+    printf("Quanta memoria ROM ha il tuo telefono?(GB)\n");
+    scanf("%d", &S2->memoriaGB);
+    printf("Quanta memoria RAM ha il tuo telefono?(GB)\n");
+    scanf("%d", &S2->ramGB);
+    printf("Quanto pesa il tuo telefono?(g)\n");
+    scanf("%lf", &S2->peso);
+    printf("\n");
+}
