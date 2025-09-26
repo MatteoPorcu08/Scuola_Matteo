8b18a2dbb3c63e672ba9e322fea83d51f84a013b
MatteoPorcu08 <porcumatteo@asproni-fermi.edu.it>
2025-09-26 09:34:13 +0200

    Commit automatico 26/09/2025  9:34:13,00

--- Informatica/Programmazione/C/Scheda Smartphone.C
+++ Informatica/Programmazione/C/Scheda Smartphone.C
@@ -13,7 +13,7 @@ int main(){
     
     Smartphone S1,S2;
 
-    //chiede di inserire i dati del primo telfono
+    //input i dati del primo telfono
     printf("Inserisci i dati del primo telefono:\n");
     printf("Che modello e' il tuo telefono?\n");
     scanf("%[^\n]s",S1.modello);
@@ -27,10 +27,10 @@ int main(){
     scanf("%lf", &S1.peso);
     
 
-    //chiede di inserire i dati del secondo telefono
+    //input dati del secondo telefono
     printf("Inserisci i dati del secondo telefono:\n");
     printf("Che modello e' il tuo telefono?\n");
-    scanf("%s[^\n]%*c",S2.modello);
+    scanf("%[^\n]s",S2.modello);
     printf("Che marca e' il tuo telefono?\n");
     scanf("%s",S2.marca);
     printf("Quanta memoria ROM ha il tuo telefono?\n");
@@ -39,6 +39,10 @@ int main(){
     scanf("%d", &S2.ramGB);
     printf("Quanto pesa il tuo telefono?\n");
     scanf("%lf", &S2.peso);
+
+    //stampa dati inseriti
+
+    
     
 
 
