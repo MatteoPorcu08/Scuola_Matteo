fdcaa27e48277d5011f9fc049aa7e5b99037449c
MatteoPorcu08 <porcumatteo@asproni-fermi.edu.it>
2025-10-06 09:49:24 +0200

    Commit automatico 06/10/2025  9:49:24,77

--- Informatica/Programmazione/Java/calcolatrice/calcolo/main.java
+++ Informatica/Programmazione/Java/calcolatrice/calcolo/main.java
@@ -3,9 +3,16 @@ package calcolatrice.calcolo;
 public class main {
     int a = 10;
     int b = 20;
+
     int somma = a + b;
     int sottrazione = a - b;
     int moltiplicazione = a * b;
     int divisione = a / b;
 
+    public static void main(String[] args) {
+        System.out.println("Il risultato della somma è:" + somma);
+        System.out.println("Il risultato della sottrazione è:" + sottrazione);
+        System.out.println("Il risultato della moltiplicazione è:" + moltiplicazione);
+        System.out.println("Il risultato della divisione è:" + divisione);
+    }
 }
