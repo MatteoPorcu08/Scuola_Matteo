e2a25a13e134f39120289eeef06dd4f9cc4654cf
MatteoPorcu08 <porcumatteo@asproni-fermi.edu.it>
2025-10-06 09:14:14 +0200

    Commit automatico 06/10/2025  9:14:13,96

--- Informatica/Programmazione/Java/Main.java
+++ Informatica/Programmazione/Java/Main.java
@@ -1,6 +1,13 @@
 public class Main {
-    int a;
-    int c;
-    float n;a=2;c=5;m=(a+c)/2;System.out.println(m);
+
+    public static void main(String[] var0) {
+        int a;
+        int c;
+        float m;
+        a = 2;
+        c = 5;
+        m = (a + c) / 2;
+        System.out.println(m);
+    }
 
 }
