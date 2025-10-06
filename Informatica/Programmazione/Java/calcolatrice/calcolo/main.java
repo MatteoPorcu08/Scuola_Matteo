9ef1fcedd9b1b6f9d24aee7e20a24aaa36bbf703
MatteoPorcu08 <porcumatteo@asproni-fermi.edu.it>
2025-10-06 09:54:26 +0200

    Commit automatico 06/10/2025  9:54:26,89

--- Informatica/Programmazione/Java/calcolatrice/calcolo/main.java
+++ Informatica/Programmazione/Java/calcolatrice/calcolo/main.java
@@ -10,7 +10,7 @@ public class main {
     int divisione = a / b;
 
     public static void main(String[] args) {
-        System.out.println("Il risultato della somma è:" + somma);
+        System.out.println("Il risultato della somma di " + a + "e "+b+:" + somma);
         System.out.println("Il risultato della sottrazione è:" + sottrazione);
         System.out.println("Il risultato della moltiplicazione è:" + moltiplicazione);
         System.out.println("Il risultato della divisione è:" + divisione);
