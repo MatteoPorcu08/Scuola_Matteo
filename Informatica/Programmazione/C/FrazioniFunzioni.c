c3bf8c7fd65485833c023306cfe59694611d46f3
MatteoPorcu08 <porcumatteo@asproni-fermi.edu.it>
2025-10-06 10:19:36 +0200

    Commit automatico 06/10/2025 10:19:36,84

--- Informatica/Programmazione/C/FrazioniFunzioni.c
+++ Informatica/Programmazione/C/FrazioniFunzioni.c
@@ -2,69 +2,45 @@
 #include <string.h>
 #include <stdlib.h>
 
-typedef struct {
+// Definizione della struttura per rappresentare una frazione
+typedef struct{
     int numeratore;
     int denominatore;
 } Frazione;
 
-/* Funzioni aritmetiche */
-Frazione moltiplica(Frazione a, Frazione b) {
-    Frazione r;
-    r.numeratore = a.numeratore * b.numeratore;
-    r.denominatore = a.denominatore * b.denominatore;
-    return r;
-}
+// funzioni per eseguire i calcoli
+void inserisci(Frazione F1);
+Frazione 
 
-Frazione dividi(Frazione a, Frazione b) {
-    Frazione r;
-    r.numeratore = a.numeratore * b.denominatore;
-    r.denominatore = a.denominatore * b.numeratore;
-    return r;
-}
-
-Frazione somma(Frazione a, Frazione b) {
-    Frazione r;
-    r.numeratore = a.numeratore * b.denominatore + b.numeratore * a.denominatore;
-    r.denominatore = a.denominatore * b.denominatore;
-    return r;
-}
-
-Frazione sottrai(Frazione a, Frazione b) {
-    Frazione r;
-    r.numeratore = a.numeratore * b.denominatore - b.numeratore * a.denominatore;
-    r.denominatore = a.denominatore * b.denominatore;
-    return r;
-}
-
-/* Funzioni basilari di I/O */
-Frazione inserisci_frazione(const char *nome) {
-    Frazione f;
-    printf("Inserisci il numeratore della %s frazione: ", nome);
-    scanf("%d", &f.numeratore);
-    do {
-        printf("Inserisci il denominatore della %s frazione (diverso da 0): ", nome);
-        scanf("%d", &f.denominatore);
-        if (f.denominatore == 0) {
-            printf("Il denominatore non puo' essere 0. Riprova.\n");
-        }
-    } while (f.denominatore == 0);
-}
-
-void stampa_frazione(const char *msg, Frazione f) {
-    printf("%s%d/%d\n", msg, f.numeratore, f.denominatore);
-}
-
-int main(void) {
+int main(){
     Frazione f1, f2, risultato;
     int scelta;
     char continua;
 
-    /* Inserimento frazioni */
-    f1 = inserisci_frazione("prima");
-    f2 = inserisci_frazione("seconda");
+        // Inserimento della prima frazione
+        printf("Inserisci il numeratore della prima frazione: ");
+        scanf("%d", &f1.numeratore);
+        do{
+            printf("Inserisci il denominatore della prima frazione (diverso da 0): ");
+            scanf("%d", &f1.denominatore);
+            if (f1.denominatore == 0){
+                printf("Il denominatore non puo' essere 0. Riprova.\n");
+            }
+        } while (f1.denominatore == 0);
 
-    do {
-        /* Menu */
+        // Inserimento della seconda frazione
+        printf("Inserisci il numeratore della seconda frazione: ");
+        scanf("%d", &f2.numeratore);
+        do{
+            printf("Inserisci il denominatore della seconda frazione (diverso da 0): ");
+            scanf("%d", &f2.denominatore);
+            if (f2.denominatore == 0){
+                printf("Il denominatore non puo' essere 0. Riprova.\n");
+            }
+        } while (f2.denominatore == 0);
+
+    do{
+        // Scelta dell'operazione
         printf("Scegli l'operazione da eseguire:\n");
         printf("1) Moltiplicazione delle frazioni\n");
         printf("2) Divisione tra due frazioni\n");
@@ -73,32 +49,44 @@ int main(void) {
         printf("Scelta: ");
         scanf("%d", &scelta);
 
-        /* Operazioni */
-        switch (scelta) {
-            case 1:
-                risultato = moltiplica(f1, f2);
-                stampa_frazione("Risultato della moltiplicazione: ", risultato);
-                break;
-            case 2:
-                risultato = dividi(f1, f2);
-                stampa_frazione("Risultato della divisione: ", risultato);
-                break;
-            case 3:
-                risultato = somma(f1, f2);
-                stampa_frazione("Risultato dell'addizione: ", risultato);
-                break;
-            case 4:
-                risultato = sottrai(f1, f2);
-                stampa_frazione("Risultato della sottrazione: ", risultato);
-                break;
-            default:
-                printf("Scelta non valida.\n");
-                break;
+        // Esecuzione delle operazioni direttamente nel main
+        switch (scelta){
+        case 1:
+            // Moltiplicazione
+            risultato.numeratore = f1.numeratore * f2.numeratore;
+            risultato.denominatore = f1.denominatore * f2.denominatore;
+            printf("Risultato della moltiplicazione: %d/%d\n", risultato.numeratore, risultato.denominatore);
+            break;
+            
+        case 2:
+            // Divisione
+            risultato.numeratore = f1.numeratore * f2.denominatore;
+            risultato.denominatore = f1.denominatore * f2.numeratore;
+            printf("Risultato della divisione: %d/%d\n", risultato.numeratore, risultato.denominatore);
+            break;
+            
+        case 3:
+            // Addizione
+            risultato.numeratore = (f1.numeratore * f2.denominatore) + (f2.numeratore * f1.denominatore);
+            risultato.denominatore = f1.denominatore * f2.denominatore;
+            printf("Risultato dell'addizione: %d/%d\n", risultato.numeratore, risultato.denominatore);
+            break;
+            
+        case 4:
+            // Sottrazione
+            risultato.numeratore = (f1.numeratore * f2.denominatore) - (f2.numeratore * f1.denominatore);
+            risultato.denominatore = f1.denominatore * f2.denominatore;
+            printf("Risultato della sottrazione: %d/%d\n", risultato.numeratore, risultato.denominatore);
+            break;
+            
+        default:
+            printf("Scelta non valida.\n");
+            break;
         }
-
+        
         printf("Vuoi eseguire un'altra operazione? (s/n): ");
-        scanf(" %c", &continua);
+        scanf("%s", &continua);
     } while (continua == 's' || continua == 'S');
-
+    
     return 0;
 }
