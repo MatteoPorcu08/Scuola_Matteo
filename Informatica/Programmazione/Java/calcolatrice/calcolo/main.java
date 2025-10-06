dd954ea7debddb3c6f15c233f0b4425dc762f467
MatteoPorcu08 <porcumatteo@asproni-fermi.edu.it>
2025-10-06 09:44:22 +0200

    Commit automatico 06/10/2025  9:44:22,79

---
+++ Informatica/Programmazione/Java/calcolatrice/calcolo/main.java
@@ -0,0 +1,11 @@
+package calcolatrice.calcolo;
+
+public class main {
+    int a = 10;
+    int b = 20;
+    int somma = a + b;
+    int sottrazione = a - b;
+    int moltiplicazione = a * b;
+    int divisione = a / b;
+
+}
