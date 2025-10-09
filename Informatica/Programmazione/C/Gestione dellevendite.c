25a83c658cbfe3c254503d96da45e0f7b6b896c1
MatteoPorcu08 <porcumatteo@asproni-fermi.edu.it>
2025-10-09 09:51:35 +0200

    Commit automatico 09/10/2025  9:51:34,96

--- Informatica/Programmazione/C/Gestione dellevendite.c
+++ Informatica/Programmazione/C/Gestione dellevendite.c
@@ -2,3 +2,13 @@
 #include <stdlib.h>
 #include <string.h>
 
+typedef struct{
+    char codice_vendita[10];
+    char nome_prodotto[50];
+    float prezzo_prodotto;
+    int quantita_venduta;
+} Prodotto;
+
+int main(){
+
+}
