d0ea77c8f16edbf73bf8f26da3f9a1f4e81e24a8
MatteoPorcu08 <porcumatteo@asproni-fermi.edu.it>
2025-10-10 12:28:53 +0200

    Commit automatico 10/10/2025 12:28:53,81

--- TPSIT/Programmazione/Esercitazioni Scuola/Esercitazione 1  10-10-2025/javascript/script.js
+++ TPSIT/Programmazione/Esercitazioni Scuola/Esercitazione 1  10-10-2025/javascript/script.js
@@ -1,3 +1,6 @@
-let num1;
-let num2;
-let num3;
+alert("Programma");
+let num1 = prompt ("Inserisci il primo numero");
+let num2 = prompt ("Inserisci il secondo numero");
+let num3 = prompt ("Inserisic il terzo numero");
+
+let scelta = prompt ("Se vuoi eseguire la somma premi (1)\n Se vuoi eseguire la sottrazione premi(2)")
