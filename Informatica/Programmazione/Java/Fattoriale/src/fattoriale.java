91f599ecf1fad18a9c36e4890c204481d2fb2344
MatteoPorcu08 <porcumatteo@asproni-fermi.edu.it>
2025-10-15 20:27:51 +0200

    Commit automatico 15/10/2025 20:27:51,57

--- Informatica/Programmazione/Java/Fattoriale/src/fattoriale.java
+++ Informatica/Programmazione/Java/Fattoriale/src/fattoriale.java
@@ -1,13 +1,10 @@
-/*Realizzare un programma che sia in grado di calcolare il fattoriale di numero che viene memorizzato nella variabile varA e stampi a schermo il valore.
-N.B. 
-Il fattoriale di un numero si trova moltiplicando tutti i numeri naturali che lo precedono (escluso lo zero), con esso incluso. 
-Ad esempio, il fattoriale del numero 4 "4!" darà come risultato "4 x 3 x 2 x 1 = 24" ed il fattoriale del numero 9 "9!" è uguale a "9 x 8 x 7 x 6 x 5 x 4 x 3 x 2 x 1 = 362.880" */
-
 public class fattoriale {
     public static void main(String[] args){
         int varA = 5;
         int fattoriale = 1;
 
+        
+        // Calcolo del fattoriale
         for(int i = varA; i > 0; i--){
             fattoriale = fattoriale * i;
         }
