ac4aae5ece65bd011ee96e148b95e00187bea643
MatteoPorcu08 <porcumatteo@asproni-fermi.edu.it>
2025-10-15 20:32:56 +0200

    Commit automatico 15/10/2025 20:32:56,24

--- Informatica/Programmazione/Java/Fattoriale/src/fattoriale.java
+++ Informatica/Programmazione/Java/Fattoriale/src/fattoriale.java
@@ -1,8 +1,10 @@
 public class fattoriale {
     public static void main(String[] args){
-        int varA = 5;
+        int varA;
         int fattoriale = 1;
 
+        // Input del numero
+        varA = input.nextInt("Inserisci un numero intero positivo: ");
         
         // Calcolo del fattoriale
         for(int i = varA; i > 0; i--){
