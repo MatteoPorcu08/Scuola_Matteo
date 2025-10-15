409c719e0f46a383a1497fc50422c6a12ec3c7c7
MatteoPorcu08 <porcumatteo@asproni-fermi.edu.it>
2025-10-15 20:38:00 +0200

    Commit automatico 15/10/2025 20:38:00,26

--- Informatica/Programmazione/Java/Fattoriale/src/fattoriale.java
+++ Informatica/Programmazione/Java/Fattoriale/src/fattoriale.java
@@ -1,10 +1,13 @@
+import java.util.Scanner; // Importa la classe Scanner per l'input da tastiera
 public class fattoriale {
     public static void main(String[] args){
-        int varA;
         int fattoriale = 1;
 
         // Input del numero
-        varA = input.nextInt("Inserisci un numero intero positivo: ");
+        Scanner input = new Scanner(System.in); // Crea un oggetto Scanner per leggere l'input
+        System.out.print("Inserisci un numero intero positivo: "); // Prompt per l'utente
+        int varA = input.nextInt(); // Legge un numero intero dall'input
+
         
         // Calcolo del fattoriale
         for(int i = varA; i > 0; i--){
