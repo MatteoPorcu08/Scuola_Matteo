c5d2973ace55a31274e2ba8a7aa001d08cde51dd
MatteoPorcu08 <porcumatteo@asproni-fermi.edu.it>
2025-10-17 13:22:56 +0200

    Commit automatico 17/10/2025 13:22:56,84

--- TPSIT/Programmazione/Esercitazioni Scuola/Esercitazione 17-10-2025/javascript/script1.js
+++ TPSIT/Programmazione/Esercitazioni Scuola/Esercitazione 17-10-2025/javascript/script1.js
@@ -1,9 +1,11 @@
-let nome = document.querySelector("#nameInput").value;
-let eta = document.querySelector("#ageInput").value;
+function checkAge() {
+let nome = document.getElementById("nameInput").value;
+let eta = document.getElementById("ageInput").value;
 
 if (eta < 18) {
   alert("Ciao " + nome + ", sei troppo giovane per registrarti");
 }
 if (eta >= 18) {
   alert("Ciao " + nome + ", registrazione completata");
+}
 }
