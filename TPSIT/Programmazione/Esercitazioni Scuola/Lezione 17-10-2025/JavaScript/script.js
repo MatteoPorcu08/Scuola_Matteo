20253ef858f58a6faee3332262a31ee14ac2895d
MatteoPorcu08 <porcumatteo@asproni-fermi.edu.it>
2025-10-17 12:29:10 +0200

    Commit automatico 17/10/2025 12:29:10,86

--- TPSIT/Programmazione/Esercitazioni Scuola/Lezione 17-10-2025/JavaScript/script.js
+++ TPSIT/Programmazione/Esercitazioni Scuola/Lezione 17-10-2025/JavaScript/script.js
@@ -1,12 +1,15 @@
-let password = prompt("Inserisci la password:");
-let codice = prompt("Inserisci il codice segreto:");
+function verifica() {
+    let password = prompt("Inserisci la password:");
+    let codice = prompt("Inserisci il codice segreto:");
 
-if (password == "admin" && codice == "0000") {
-  alert("Accesso speciale");
+    if (password == "admin" && codice == "0000") {
+    alert("Accesso speciale");
+    }
+    if (password == "admin" && codice != "0000") {
+    alert("Codice errato");
+    }
+    if (password != "admin") {
+    alert("Accesso negato");
+    }
 }
-if (password == "admin" && codice != "0000") {
-  alert("Codice errato");
-}
-if (password != "admin") {
-  alert("Accesso negato");
-}
+
