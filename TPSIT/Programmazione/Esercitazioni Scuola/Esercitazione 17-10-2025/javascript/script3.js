b2fbddc4168eaed9a86cdf9c19b7a61fb606e08b
MatteoPorcu08 <porcumatteo@asproni-fermi.edu.it>
2025-10-18 18:52:34 +0200

    Commit automatico 18/10/2025 18:52:34,01

--- TPSIT/Programmazione/Esercitazioni Scuola/Esercitazione 17-10-2025/javascript/script3.js
+++ TPSIT/Programmazione/Esercitazioni Scuola/Esercitazione 17-10-2025/javascript/script3.js
@@ -1,19 +1,16 @@
 function calcola() {
-    const copiesStr = document.getElementById("copiesInput").value;
-    const costStr = document.getElementById("costInput").value;
+  const copiesStr = document.getElementById("copiesInput").value;
+  const costStr = document.getElementById("costInput").value;
 
-    // conversione semplice
-    const copies = parseFloat(copiesStr);
-    const cost = parseFloat(costStr);
+  const copies = parseFloat(copiesStr);
+  const cost = parseFloat(costStr);
 
-    // importo iniziale
-    const importo = copies * cost;
-    alert(`Importo da pagare: ${importo.toFixed(2)} euro`);
+  const importo = copies * cost;
+  alert(`Importo da pagare: ${importo.toFixed(2)} euro`);
 
-    // sconto via prompt
-    const scontoStr = prompt("Inserisci lo sconto percentuale da applicare (es. 5):");
-    const sconto = parseFloat(scontoStr);
+  const scontoStr = prompt("Inserisci lo sconto percentuale da applicare (es. 5):");
+  const sconto = parseFloat(scontoStr);
 
-    const nuovoImporto = importo * (1 - sconto / 100);
-    alert(`Con il ${sconto}% di sconto dovrai pagare ${nuovoImporto.toFixed(2)} euro`);
-  }
+  const nuovoImporto = importo * (1 - sconto / 100);
+  alert(`Con il ${sconto}% di sconto dovrai pagare ${nuovoImporto.toFixed(2)} euro`);
+}
