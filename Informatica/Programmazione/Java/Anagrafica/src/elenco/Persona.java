b162000ea8cbb7b4a8f13fffe5d47f40ece309c9
MatteoPorcu08 <porcumatteo@asproni-fermi.edu.it>
2025-10-18 11:51:35 +0200

    Commit automatico 18/10/2025 11:51:34,95

---
+++ Informatica/Programmazione/Java/Anagrafica/src/elenco/Persona.java
@@ -0,0 +1,10 @@
+package elenco;
+
+public class Persona {
+    // Attributi: nome, cognome, età, altezza
+    private String nome;
+    private String cognome;
+    private int età;
+    private double altezza;
+
+}
