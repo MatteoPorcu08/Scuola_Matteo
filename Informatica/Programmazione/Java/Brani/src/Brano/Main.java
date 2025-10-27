5414e4be3c9e103e2edceacc4974b305f56b8b38
MatteoPorcu08 <porcumatteo@asproni-fermi.edu.it>
2025-10-27 09:30:09 +0100

    Commit automatico 27/10/2025  9:30:09,86

--- Informatica/Programmazione/Java/Brani/src/Brano/Main.java
+++ Informatica/Programmazione/Java/Brani/src/Brano/Main.java
@@ -1,6 +1,15 @@
 package brano;
 
 public class Main {
-    
+    public static void main(String[] args) {
+        Canzone canzone1 = new Canzone("Blinding Lights", "The Weeknd", 200, "Pop");
+        Canzone canzone2 = new Canzone("Bohemian Rhapsody", "Queen", 354, "Rock");
+        Canzone canzone3 = new Canzone("Bad Guy", "Billie Eilish", 194, "Eletronica");
+        Canzone canzone4 = new Canzone("Lose Yourself", "John Lennon", 183, "Pop");
+        Canzone canzone5 = new Canzone("Shape of You", "Ed Sheeran", 233, "Pop");
+
+
+      
+    }
 
 }
