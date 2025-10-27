c67c419abc2e44debf6820c20e4837229130eaf8
MatteoPorcu08 <porcumatteo@asproni-fermi.edu.it>
2025-10-27 10:00:28 +0100

    Commit automatico 27/10/2025 10:00:28,90

--- Informatica/Programmazione/Java/Brani/src/Brano/Main.java
+++ Informatica/Programmazione/Java/Brani/src/Brano/Main.java
@@ -7,45 +7,13 @@ public class Main {
         Canzone canzone4 = new Canzone("Lose Yourself", "Eminem", 326, "Rap");
         Canzone canzone5 = new Canzone("Shape of You", "Ed Sheeran", 233, "Pop");
 
-    //Stampa Detagli Canzoni
-    stampaDettagli(canzone1);
-
-    //Simulazione Riproduzioni
-        canzone1.riproduci();
-        canzone1.riproduci();
-        canzone2.riproduci();
-        canzone3.riproduci();
-        canzone3.riproduci();
-        canzone3.riproduci();
-        canzone4.riproduci();
-        canzone5.riproduci();
-        canzone5.riproduci();
-        canzone2.riproduci();
-
-    //Test getter
-        System.out.println("\nNumero di riproduzioni di '" + canzone1.getTitolo() + "': " + canzone1.getRiproduzioni());
-        System.out.println("Durata formattata di '" + canzone2.getTitolo() + "': " + canzone2.getDurataFormattata());
-        System.out.println("Categoria durata di '" + canzone3.getTitolo() + "': " + canzone3.getCategoriaDurata());
-
-    //Durata formattata
-        System.out.println("\nDurata formattata di tutte le canzoni:");
-        System.out.println(canzone1.getTitolo() + ": " + canzone1.getDurataFormattata());
-        System.out.println(canzone2.getTitolo() + ": " + canzone2.getDurataFormattata());
-        System.out.println(canzone3.getTitolo() + ": " + canzone3.getDurataFormattata());
-        System.out.println(canzone4.getTitolo() + ": " + canzone4.getDurataFormattata());
-        System.out.println(canzone5.getTitolo() + ": " + canzone5.getDurataFormattata());
-    
-    //ToString
-        System.out.println("\nDettagli completi delle canzoni:");
-        System.out.println(canzone1);
-        System.out.println();
-        System.out.println(canzone2);
-        System.out.println();
-        System.out.println(canzone3);
-        System.out.println();
-        System.out.println(canzone4);
-        System.out.println();
-        System.out.println(canzone5);
-
+    //Stampa dettagli canzoni
+        System.out.println(canzone1.stampaDettagli());
+        System.out.println(canzone2.stampaDettagli());
+        System.out.println(canzone3.stampaDettagli());
+        System.out.println(canzone4.stampaDettagli());
+        System.out.println(canzone5.stampaDettagli());
     }
+
+    
 }
