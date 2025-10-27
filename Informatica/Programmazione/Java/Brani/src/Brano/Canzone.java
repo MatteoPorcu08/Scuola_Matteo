9177f1b54b15166da5c94d29fcbafbf8b096dde1
MatteoPorcu08 <porcumatteo@asproni-fermi.edu.it>
2025-10-27 09:15:03 +0100

    Commit automatico 27/10/2025  9:15:03,79

---
+++ Informatica/Programmazione/Java/Brani/src/Brano/Canzone.java
@@ -0,0 +1,74 @@
+package Brano;
+public class Canzone {
+    private String titolo;
+    private String artista;
+    private int durata; // durata in secondi
+    private String genere;
+    private int riproduzioni;
+
+    // Costruttore
+    public Canzone (String titolo, String artista, int durata, String genere) {
+        this.titolo = titolo;
+        this.artista = artista;
+        this.durata = durata;
+        this.genere = genere;
+        this.riproduzioni = 0;
+    }
+
+    //Setter
+    private void setTitolo(String titolo) {
+        this.titolo = titolo;
+    }
+    private void setArtista(String artista) {
+        this.artista = artista;
+    }
+    private void setDurata(int durata) {
+        this.durata = durata;
+    }
+    private void setGenere(String genere) {
+        this.genere = genere;
+    }
+    private void setRiproduzioni(int riproduzioni) {
+        this.riproduzioni = riproduzioni;
+    }
+
+    //Getter
+    public String getTitolo() {
+        return titolo;
+    }
+    public String getArtista() {
+        return artista;
+    }
+    public int getDurata() {
+        return durata;
+    }
+    public String getGenere() {
+        return genere;
+    }
+    public int getRiproduzioni() {
+        return riproduzioni;
+    }
+
+    //Metodi Specifici
+    void riproduci() {
+        riproduzioni++;
+        System.out.println("Riproducendo: " + titolo + " di " + artista);
+    }
+    String getDurataFormattata() {
+        int minuti = durata / 60;
+        int secondi = durata % 60;
+        return String.format("%02d:%02d", minuti, secondi);
+    }
+    String stampaDettagli() {
+        return "Titolo: " + titolo + "\nArtista: " + artista + "\nDurata: " + getDurataFormattata() + "\nGenere: " + genere + "\nRiproduzioni: " + riproduzioni;
+    }
+    String getCategoriaDurata(){
+        if(durata < 180){
+            return "Canzone Breve";
+        } else if(durata <= 300){
+            return "Canzone Media";
+        } else {
+            return "Canzone Lunga";
+        }
+    }
+}
