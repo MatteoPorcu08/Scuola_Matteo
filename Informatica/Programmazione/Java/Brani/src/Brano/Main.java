c0e8aeb8506033c18f4e18eb6c3bc3c2da15e459
MatteoPorcu08 <porcumatteo@asproni-fermi.edu.it>
2025-10-27 09:35:11 +0100

    Commit automatico 27/10/2025  9:35:11,90

--- Informatica/Programmazione/Java/Brani/src/Brano/Main.java
+++ Informatica/Programmazione/Java/Brani/src/Brano/Main.java
@@ -5,11 +5,22 @@ public class Main {
         Canzone canzone1 = new Canzone("Blinding Lights", "The Weeknd", 200, "Pop");
         Canzone canzone2 = new Canzone("Bohemian Rhapsody", "Queen", 354, "Rock");
         Canzone canzone3 = new Canzone("Bad Guy", "Billie Eilish", 194, "Eletronica");
-        Canzone canzone4 = new Canzone("Lose Yourself", "John Lennon", 183, "Pop");
+        Canzone canzone4 = new Canzone("Lose Yourself", "Eminem", 326, "Rap");
         Canzone canzone5 = new Canzone("Shape of You", "Ed Sheeran", 233, "Pop");
 
+    //Stampa Detagli Canzoni
+        stampaDettagli(canzone1);
+        stampaDettagli(canzone2);
+        stampaDettagli(canzone3);
+        stampaDettagli(canzone4);
+        stampaDettagli(canzone5);
 
-      
+    //Simulazione Riproduzioni
+
+
+
+
+
     }
 
 }
