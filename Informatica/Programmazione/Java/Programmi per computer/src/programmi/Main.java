a18478633089c75b5de984ea2b7bfa722494d93c
MatteoPorcu08 <porcumatteo@asproni-fermi.edu.it>
2025-10-28 20:34:07 +0100

    Commit automatico 28/10/2025 20:34:07,02

--- Informatica/Programmazione/Java/Programmi per computer/src/programmi/Main.java
+++ Informatica/Programmazione/Java/Programmi per computer/src/programmi/Main.java
@@ -5,7 +5,7 @@ public class Main {
         ProgrammiComputer programma1 = new ProgrammiComputer("AutoCAD", "Autodesk", "1.0", "Windows", 1982);
         ProgrammiComputer programma2 = new ProgrammiComputer("Visual Studio Code", "Microsoft", "2.0", "Linux", 2015);
 
-        // Stampa le informazioni sui programmi
+        // Confronto tra i due programmi
         int confronto = programma1.compareAnno(programma2);
         if (confronto < 0) {
             System.out.println(programma1.getDenominazione() + " è stato rilasciato prima di " + programma2.getDenominazione());
@@ -15,6 +15,10 @@ public class Main {
             System.out.println("Entrambi i programmi sono stati rilasciati nello stesso anno.");
         }
 
-
+        //Metodo toString
+        System.out.println("\nDettagli Programma 1:");
+        System.out.println(programma1.toString());
+        System.out.println("\nDettagli Programma 2:");
+        System.out.println(programma2.toString());
     }
 }
