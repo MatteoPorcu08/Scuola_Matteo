ac500c238b0544f516649ca0e2e0f65d794d8ef3
MatteoPorcu08 <porcumatteo@asproni-fermi.edu.it>
2025-10-28 13:26:35 +0100

    Commit automatico 28/10/2025 13:26:35,15

--- Informatica/Programmazione/Java/Programmi per computer/src/programmi/ProgrammiComputer.java
+++ Informatica/Programmazione/Java/Programmi per computer/src/programmi/ProgrammiComputer.java
@@ -1,4 +1,39 @@
 package programmi;
+
 public class ProgrammiComputer {
+    //denominazione, produttore, versione, sistema operativo, anno
+    private String denominazione;
+    private String produttore;
+    private String versione;
+    private String sistemaOperativo;
+    private int anno;
+
+    //costruttore
+    public ProgrammiComputer(String denominazione, String produttore, String versione, String sistemaOperativo, int anno) {
+        this.denominazione = denominazione;
+        this.produttore = produttore;
+        this.versione = versione;
+        this.sistemaOperativo = sistemaOperativo;
+        this.anno = anno;
+    }
     
+    //getter
+    public String getDenominazione() {
+        return denominazione;
+    }
+    public String getProduttore() {
+        return produttore;
+    }
+    public String getVersione() {
+        return versione;
+    }
+    public String getSistemaOperativo() {
+        return sistemaOperativo;
+    }
+    public int getAnno() {
+        return anno;
+    }
+
+    //setter
+
 }
