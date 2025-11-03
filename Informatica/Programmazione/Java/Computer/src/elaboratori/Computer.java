f318605f2a3ad5c58764cfe5ad3a969671e111be
MatteoPorcu08 <porcumatteo@asproni-fermi.edu.it>
2025-11-03 09:30:57 +0100

    Commit automatico 03/11/2025  9:30:57,91

--- Informatica/Programmazione/Java/Computer/src/elaboratori/Computer.java
+++ Informatica/Programmazione/Java/Computer/src/elaboratori/Computer.java
@@ -1,8 +1,61 @@
 package elaboratori;
 public class Computer {
+    private String marca;
     private String modello;
-    private String produttore;
-    private String versione;
-    private String sistemaOperativo;
-    private int anno;
+    private double  prezzo;
+    private String cpu;
+    private int ram;
+    private int storage;
+
+    //costruttore
+    public Computer(String marca, String modello, double prezzo, String cpu, int ram, int storage) {
+        this.marca = marca;
+        this.modello = modello;
+        this.prezzo = prezzo;
+        this.cpu = cpu;
+        this.ram = ram;
+        this.storage = storage;
+}
+
+//getter
+public String getMarca(){
+    return marca;
+}
+public String getModello(){
+    return modello;
+}
+public double getPrezzo(){
+    return prezzo;
+}
+public String getCpu(){
+    return cpu;
+}
+public int getRam(){
+    return ram;
+}
+public int getStorage(){
+    return storage;
+}
+
+//setter
+private void setMarca(String marca){
+    this.marca = marca;
+}
+private void setModello(String modello){
+    this.modello = modello;
+}
+private void setPrezzo(double prezzo){
+    this.prezzo = prezzo;
+}
+private void setCpu(String cpu){
+    this.cpu = cpu;
+}
+private void setRam(int ram){
+    this.ram = ram;
+}
+private void setStorage(int storage){
+    this.storage = storage;
+}
+
+
 }
