5680c5dd17f837cabd080ab2648b16c6c7a425fe
MatteoPorcu08 <porcumatteo@asproni-fermi.edu.it>
2025-11-03 10:25:40 +0100

    Commit automatico 03/11/2025 10:25:40,02

--- Informatica/Programmazione/Java/Computer/src/elaboratori/Main.java
+++ Informatica/Programmazione/Java/Computer/src/elaboratori/Main.java
@@ -1,6 +1,7 @@
 package elaboratori;
 public class Main {
     public static void main(String[] args) {
-        Computer computer1=new computerDesktop("Dell","Intel i7",16,512,true);
+        elaboratori pc1 = new elaboratori("Dell", "XPS 13", 16, 512);
+        elaboratori pc2 = new elaboratori("Apple", "MacBook Pro", 32, 1024);
     }
 }
