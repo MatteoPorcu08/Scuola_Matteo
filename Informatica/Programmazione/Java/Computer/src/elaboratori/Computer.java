1d5a8a22117aa846e952db933721867eea5a1f39
MatteoPorcu08 <porcumatteo@asproni-fermi.edu.it>
2025-11-03 09:25:55 +0100

    Commit automatico 03/11/2025  9:25:55,83

---
+++ Informatica/Programmazione/Java/Computer/src/elaboratori/Computer.java
@@ -0,0 +1,8 @@
+package elaboratori;
+public class Computer {
+    private String modello;
+    private String produttore;
+    private String versione;
+    private String sistemaOperativo;
+    private int anno;
+}
