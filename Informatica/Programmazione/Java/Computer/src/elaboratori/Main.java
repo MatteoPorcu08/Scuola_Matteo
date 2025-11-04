3a526802fb89899fcd2f17623c9faf785ded4826
MatteoPorcu08 <porcumatteo@asproni-fermi.edu.it>
2025-11-04 12:47:33 +0100

    Commit automatico 04/11/2025 12:47:33,35

--- Informatica/Programmazione/Java/Computer/src/elaboratori/Main.java
+++ Informatica/Programmazione/Java/Computer/src/elaboratori/Main.java
@@ -3,22 +3,28 @@ public class Main {
     public static void main(String[] args) {
         elaboratori.Computer pc1 = new elaboratori.Computer("Asus", "ZenBook", 999.99, "Intel i7", 16, 512);
         elaboratori.Computer pc2 = new elaboratori.Computer("Dell", "XPS 13", 1199.99, "Intel i5", 8, 256);
+        
+        // Stampa utilizzando il metodo toString
+        System.out.println("\nUtilizzando toString():");
+        System.out.println(pc1.toString());
+        System.out.println(pc2.toString());
+        System.out.println("\n");
 
-        // Stampa le informazioni dei computer
-        System.out.println("Computer 1:");
-        System.out.println("Marca: " + pc1.getMarca());
-        System.out.println("Modello: " + pc1.getModello());
-        System.out.println("Prezzo: " + pc1.getPrezzo());
-        System.out.println("CPU: " + pc1.getCpu());
-        System.out.println("RAM: " + pc1.getRam() + "GB");
-        System.out.println("Storage: " + pc1.getStorage() + "GB");
-        System.out.println(\n);
-        System.out.println("Computer 2:");
-        System.out.println("Marca: " + pc2.getMarca());
-        System.out.println("Modello: " + pc2.getModello());
-        System.out.println("Prezzo: " + pc2.getPrezzo());
-        System.out.println("CPU: " + pc2.getCpu());
-        System.out.println("RAM: " + pc2.getRam() + "GB");
-        System.out.println("Storage: " + pc2.getStorage() + "GB");
+        // Stampa la fascia di prezzo
+        System.out.println("\nFascia di prezzo:");
+        System.out.println("Computer 1: " + pc1.ottieniFasciaDiPrezzo());
+        System.out.println("Computer 2: " + pc2.ottieniFasciaDiPrezzo());
+        System.out.println("\n");
+
+        //stampo i metodi aggiuntivi
+        //stampo il emtodo aggiornaPrezzo
+        System.out.println("\nAggiorna Prezzo");
+        System.out.println("Computer 1 nuovo prezzo: " + pc1.aggiornaPrezzo(10) + "€");
+        System.out.println("Computer 2 nuovo prezzo: " + pc2.aggiornaPrezzo(15) + "€");
+        System.out.println("\n");
+
+        //stampo il metodo upgradeRam
+        System.out.println("\nUpgrade RAM");
+        
     }
 }
