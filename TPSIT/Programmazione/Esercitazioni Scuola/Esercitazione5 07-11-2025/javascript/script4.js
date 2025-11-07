fda47131a8a995aece052959c9f65e8729fda5cc
MatteoPorcu08 <porcumatteo@asproni-fermi.edu.it>
2025-11-07 12:49:50 +0100

    Commit automatico 07/11/2025 12:49:50,00

--- TPSIT/Programmazione/Esercitazioni Scuola/Esercitazione5 07-11-2025/javascript/script4.js
+++ TPSIT/Programmazione/Esercitazioni Scuola/Esercitazione5 07-11-2025/javascript/script4.js
@@ -1,22 +1,21 @@
-//Realizzare tramite html e Javascript una pagina web che riproduca una calcolatrice semplificata.  Nello specifico l'applicativo dovrà permettere all'utente di inserire due numeri e scegliere tramite 4 bottoni l'operazione che desidera venga effettuata (somma, prodotto, divisione, sottrazione), il risultato sarà visualizzato in pagina.
 function somma() {
     let num1 = document.getElementById("num1").value;
     let num2 = document.getElementById("num2").value;
-    let result = num1 + num2;
+    let result = num1*1 + num2*1;
     document.getElementById("result").innerHTML = "Risultato: " + result;
 }
 
 function sottrazione() {
     let num1 = document.getElementById("num1").value;
     let num2 = document.getElementById("num2").value;
-    let result = num1 - num2;
+    let result = num1*1 - num2*1;
     document.getElementById("result").innerHTML = "Risultato: " + result;
 }
 
 function moltiplicazione() {
     let num1 = document.getElementById("num1").value;
     let num2 = document.getElementById("num2").value;
-    let result = num1 * num2;
+    let result = num1*1 * num2*1;
     document.getElementById("result").innerHTML = "Risultato: " + result;
 }
 
@@ -26,7 +25,7 @@ function divisione() {
     if (num2 === 0) {
         document.getElementById("result").innerHTML = "Errore: Divisione per zero!";
     } else {
-        let result = num1 / num2;
+        let result = num1*1 / num2*1;
         document.getElementById("result").innerHTML = "Risultato: " + result;
     }
 }
