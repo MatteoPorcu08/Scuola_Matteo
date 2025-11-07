a17047fe025b9bdf9711b74581f7d6c6bc74e78f
MatteoPorcu08 <porcumatteo@asproni-fermi.edu.it>
2025-11-07 13:05:09 +0100

    Commit automatico 07/11/2025 13:05:09,54

--- TPSIT/Programmazione/Esercitazioni Scuola/Esercitazione5 07-11-2025/javascript/script4.js
+++ TPSIT/Programmazione/Esercitazioni Scuola/Esercitazione5 07-11-2025/javascript/script4.js
@@ -1,31 +1,27 @@
 function somma() {
     let num1 = Number(document.getElementById("num1").value);
     let num2 = Number(document.getElementById("num2").value);
-    let result = num1 + num2;
-    alert("Risultato somma: " + result);
+    document.getElementById("result").innerHTML = "Risultato somma: " + (num1 + num2);
 }
 
-function sottrazione() {
+function sottrai() {
     let num1 = Number(document.getElementById("num1").value);
     let num2 = Number(document.getElementById("num2").value);
-    let result = num1 - num2;
-    alert("Risultato sottrazione: " + result);
+    document.getElementById("result").innerHTML = "Risultato sottrazione: " + (num1 - num2);
 }
 
-function moltiplicazione() {
+function moltiplica() {
     let num1 = Number(document.getElementById("num1").value);
     let num2 = Number(document.getElementById("num2").value);
-    let result = num1 * num2;
-    alert("Risultato moltiplicazione: " + result);
+    document.getElementById("result").innerHTML = "Risultato moltiplicazione: " + (num1 * num2);
 }
 
-function divisione() {
+function dividi() {
     let num1 = Number(document.getElementById("num1").value);
     let num2 = Number(document.getElementById("num2").value);
     if (num2 === 0) {
-        alert("Errore: Divisione per zero!");
+        document.getElementById("result").innerHTML = "Errore: divisione per zero!";
     } else {
-        let result = num1 / num2;
-        alert("Risultato divisione: " + result);
+        document.getElementById("result").innerHTML = "Risultato divisione: " + (num1 / num2);
     }
 }
