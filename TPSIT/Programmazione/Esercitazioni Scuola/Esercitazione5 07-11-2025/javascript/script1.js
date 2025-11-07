d3f1bd5c01a38e9b8a7ab3b9915a9582e5b0ea37
MatteoPorcu08 <porcumatteo@asproni-fermi.edu.it>
2025-11-07 12:19:33 +0100

    Commit automatico 07/11/2025 12:19:33,05

---
+++ TPSIT/Programmazione/Esercitazioni Scuola/Esercitazione5 07-11-2025/javascript/script1.js
@@ -0,0 +1,9 @@
+function calcolaSomma() {
+    let somma = 0;
+    let numero1 = document.getElementsByClassName("num1");
+    let numero2 = document.getElementsByClassName("num2");
+    let risultato = document.getElementsByClassName("risultato");
+
+    somma= num1+num2;
+    risultato = "La somma è: " + somma;
+}
