0edc6d021b33a70f6ef6e1841e9ae8d4c0909e44
MatteoPorcu08 <porcumatteo@asproni-fermi.edu.it>
2025-11-10 10:07:49 +0100

    Commit automatico 10/11/2025 10:07:49,85

--- Informatica/Programmazione/Java/Catena di autonoleggio/src/Catena/CatenaAutonoleggio.java
+++ Informatica/Programmazione/Java/Catena di autonoleggio/src/Catena/CatenaAutonoleggio.java
@@ -1,4 +1,9 @@
 package  Catena;
 public class CatenaAutonoleggio {
-    
+    private String targa;
+    private String marca;
+    private String modello;
+    private int cilindrata;
+    private int annoacquisto;
+    private int numeroposti;
 }
