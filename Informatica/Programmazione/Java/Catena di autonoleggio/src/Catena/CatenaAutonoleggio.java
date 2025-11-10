7fa1269c06d1e0ae336999b00e929a4c109cd047
MatteoPorcu08 <porcumatteo@asproni-fermi.edu.it>
2025-11-10 10:12:51 +0100

    Commit automatico 10/11/2025 10:12:51,85

--- Informatica/Programmazione/Java/Catena di autonoleggio/src/Catena/CatenaAutonoleggio.java
+++ Informatica/Programmazione/Java/Catena di autonoleggio/src/Catena/CatenaAutonoleggio.java
@@ -6,4 +6,27 @@ public class CatenaAutonoleggio {
     private int cilindrata;
     private int annoacquisto;
     private int numeroposti;
+
+    //Costruttore
+    public CatenaAutonoleggio(String targa, String marca, String modello, int cilindrata, int annoacquisto, int numeroposti) {
+        this.targa = targa;
+        this.marca = marca;
+        this.modello = modello;
+        this.cilindrata = cilindrata;
+        this.annoacquisto = annoacquisto;
+        this.numeroposti = numeroposti;
+    }
+    
+    //Costruttore di copia
+    public CatenaAutonoleggio(CatenaAutonoleggio altraAuto) {
+        this.targa = altraAuto.targa;
+        this.marca = altraAuto.marca;
+        this.modello = altraAuto.modello;
+        this.cilindrata = altraAuto.cilindrata;
+        this.annoacquisto = altraAuto.annoacquisto;
+        this.numeroposti = altraAuto.numeroposti;
+    }
+
+    //Metodi getter
+    public String getTarga() {
 }
