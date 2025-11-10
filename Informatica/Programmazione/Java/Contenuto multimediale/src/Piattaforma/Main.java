b2c635c1c6a1362f07e73a0b9685774f91cb5e19
MatteoPorcu08 <porcumatteo@asproni-fermi.edu.it>
2025-11-10 09:17:33 +0100

    Commit automatico 10/11/2025  9:17:33,83

--- Informatica/Programmazione/Java/Contenuto multimediale/src/Piattaforma/Main.java
+++ Informatica/Programmazione/Java/Contenuto multimediale/src/Piattaforma/Main.java
@@ -1,4 +1,10 @@
 package Piattaforma;
 public class Main {
+    public static void main (String[] args) {
+        ContenutoMiltimediale A = new ContenutoMultimediale("Oppenheimer ", "Drammatico", 180, 2023, 4.5);
+        ContenutoMiltimediale B = new ContenutoMultimediale("Barbie", "Commedia", 120, 2023, 4.0);
 
+        //presentazione della piattaforma
+        System.out.println("Benvenuto nella Piattaforma di Contenuto Multimediale!");
+    }
 }
