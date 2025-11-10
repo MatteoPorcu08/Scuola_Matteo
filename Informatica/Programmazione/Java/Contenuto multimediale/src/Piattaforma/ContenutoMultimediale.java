2944243169652f7d07e1a62837e519f6d189f1d2
MatteoPorcu08 <porcumatteo@asproni-fermi.edu.it>
2025-11-10 08:52:23 +0100

    Commit automatico 10/11/2025  8:52:23,89

--- Informatica/Programmazione/Java/Contenuto multimediale/src/Piattaforma/ContenutoMultimediale.java
+++ Informatica/Programmazione/Java/Contenuto multimediale/src/Piattaforma/ContenutoMultimediale.java
@@ -5,4 +5,36 @@ public class ContenutoMultimediale {
     private int durata;//durata in minuti
     private int annoUscita;
     private double valutazione;//un punteggio da 0.0 a 5.0 che rappresenta la qualità del contenuto
+
+    //costruttore principale
+    public ContenutoMultimediale(String titolo, String genere, int durata, int annoUscita, double valutazione) {
+        this.titolo = titolo;
+        this.genere = genere;
+        this.durata = durata;
+        this.annoUscita = annoUscita;
+        this.valutazione = valutazione;
+}
+
+    //costruttore di copia
+    public ContenutoMultimediale(ContenutoMultimediale altro) {
+        this.titolo = altro.titolo;
+        this.genere = altro.genere;
+        this.durata = altro.durata;
+        this.annoUscita = altro.annoUscita;
+        this.valutazione = altro.valutazione;
+    }
+
+    //metodi getter
+    private String getTitolo() {
+        return titolo;
+        return genere;
+        return durata;
+        return annoUscita;
+        return valutazione;
+    }
+
+
+
+
+
 }
