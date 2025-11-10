5e2a3d3d110c5128b3d66b95da57ca66f94cf5e8
MatteoPorcu08 <porcumatteo@asproni-fermi.edu.it>
2025-11-10 10:30:27 +0100

    Commit automatico 10/11/2025 10:30:27,75

--- Informatica/Programmazione/Java/Catena di autonoleggio/src/Catena/Main.java
+++ Informatica/Programmazione/Java/Catena di autonoleggio/src/Catena/Main.java
@@ -1,7 +1,7 @@
 package Catena;
 public class Main {
     public static void main(String[] args) {
-        CatenaAutonoleggio v1 = new CatenaAutonoleggio("FI500EP", "Fiat", "Panda", 2015, 4);
+        CatenaAutonoleggio v1 = new CatenaAutonoleggio("FI500EP", "Fiat", "500", 1958, 4, 0);
+        CatenaAutonoleggio v2 = new CatenaAutonoleggio("BM320LX", "BMW", "Serie 3", 2023, 5, 0);
     }
-
 }
