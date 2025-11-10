0c8ba7c3a9802afb763907f9d88c247c2d0102a3
MatteoPorcu08 <porcumatteo@asproni-fermi.edu.it>
2025-11-10 08:47:20 +0100

    Commit automatico 10/11/2025  8:47:20,88

---
+++ Informatica/Programmazione/Java/Contenuto multimediale/src/Piattaforma/ContenutoMultimediale.java
@@ -0,0 +1,8 @@
+package Piattaforma;
+public class ContenutoMultimediale {
+    private String titolo;
+    private String genere;
+    private int durata;//durata in minuti
+    private int annoUscita;
+    private double valutazione;//un punteggio da 0.0 a 5.0 che rappresenta la qualità del contenuto
+}
