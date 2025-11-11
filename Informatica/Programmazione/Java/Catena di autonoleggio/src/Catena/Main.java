85fdb0b8eea3d8e1590ae20acf3238b1d548e719
MatteoPorcu08 <porcumatteo@asproni-fermi.edu.it>
2025-11-11 12:46:48 +0100

    Commit automatico 11/11/2025 12:46:48,09

--- Informatica/Programmazione/Java/Catena di autonoleggio/src/Catena/Main.java
+++ Informatica/Programmazione/Java/Catena di autonoleggio/src/Catena/Main.java
@@ -4,14 +4,34 @@ public class Main {
         CatenaAutonoleggio v1 = new CatenaAutonoleggio("FI500EP", "Fiat", "500", 499, 1958, 4);
         CatenaAutonoleggio v2 = new CatenaAutonoleggio("BM320LX", "BMW", "Serie 3", 1999, 2023, 5);
 
-        //presentazione programma
-        System.out.println("Benvenuto nel programma di gestione della catena di autonoleggio!");
+        //Presentazione programma
+        System.out.println("---Benvenuto nel programma di gestione della catena di autonoleggio!---");
         System.out.println("\n");
 
-        //visualizzazione auto
-        System.out.println("Ecco le auto attualmente disponibili nel nostro parco auto:");
+        //Visualizzazione auto toString
+        System.out.println("---Ecco le auto attualmente disponibili nel nostro parco auto---");
         System.out.println("Auto 1:");
-        System.out.println("Targa: " + v1.getTarga());
+        System.out.println(v1.toString());
+        System.out.println("Auto 2:");
+        System.out.println(v2.toString());
+        System.out.println("--------------------------------------------------");
+        System.out.println("\n");
+
+        //Costruttore di copia
+        CatenaAutonoleggio v3 = new CatenaAutonoleggio(v1);
+        System.out.println("Ecco l'auto 3, creata tramite il costruttore di copia a partire dall'auto 1:");
+        System.out.println(v3.toString());
+        System.out.println("\n");
+
+        //Metodo confrontaautopotenza
+        System.out.println("---Confronto della cilindrata tra l'auto 1 e l'auto 2---");
+        System.out.println(v1.confrontaautopotenza(v2));
+        System.out.println("--------------------------------------------------");
+        System.out.println("\n");
+
+        //Metodo nolleggio
+        System.out.println("Simulazione del noleggio dell'auto 2:");
+        
 
     }
 }
