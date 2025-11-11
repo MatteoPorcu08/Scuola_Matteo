d71bbfc3a6aaea29cbfa02248edacb496dc6042f
MatteoPorcu08 <porcumatteo@asproni-fermi.edu.it>
2025-11-11 12:41:45 +0100

    Commit automatico 11/11/2025 12:41:45,02

--- Informatica/Programmazione/Java/Catena di autonoleggio/src/Catena/Main.java
+++ Informatica/Programmazione/Java/Catena di autonoleggio/src/Catena/Main.java
@@ -1,7 +1,17 @@
 package Catena;
 public class Main {
     public static void main(String[] args) {
-        CatenaAutonoleggio v1 = new CatenaAutonoleggio("FI500EP", "Fiat", "500", 1958, 4, 0);
-        CatenaAutonoleggio v2 = new CatenaAutonoleggio("BM320LX", "BMW", "Serie 3", 2023, 5, 0);
+        CatenaAutonoleggio v1 = new CatenaAutonoleggio("FI500EP", "Fiat", "500", 499, 1958, 4);
+        CatenaAutonoleggio v2 = new CatenaAutonoleggio("BM320LX", "BMW", "Serie 3", 1999, 2023, 5);
+
+        //presentazione programma
+        System.out.println("Benvenuto nel programma di gestione della catena di autonoleggio!");
+        System.out.println("\n");
+
+        //visualizzazione auto
+        System.out.println("Ecco le auto attualmente disponibili nel nostro parco auto:");
+        System.out.println("Auto 1:");
+        System.out.println("Targa: " + v1.getTarga());
+
     }
 }
