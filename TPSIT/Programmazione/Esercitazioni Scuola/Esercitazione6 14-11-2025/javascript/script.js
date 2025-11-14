7d8d65d8a250d8d6d1daceb3cc0da280aed9f7ef
MatteoPorcu08 <porcumatteo@asproni-fermi.edu.it>
2025-11-14 12:39:48 +0100

    Commit automatico 14/11/2025 12:39:48,85

--- TPSIT/Programmazione/Esercitazioni Scuola/Esercitazione6 14-11-2025/javascript/script.js
+++ TPSIT/Programmazione/Esercitazioni Scuola/Esercitazione6 14-11-2025/javascript/script.js
@@ -2,7 +2,7 @@ function aggiungiProdotto() {
     prompt("Inserisci il nome del prodotto:");
     const carrello = document.getElementById("carrello");
     const prodotto = document.createElement("div");
-    prodotto.textContent = "Prodotto aggiunto al carrello";
-    carrello.appendChild(prodotto);
+    c++;
+    document.getElementById("carrello").innerHTML +"Alimento" + c + = +alimento;
 
 }
