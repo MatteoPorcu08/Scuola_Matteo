d62419845a7a7875b2bc45b74b747def5f6166c1
MatteoPorcu08 <porcumatteo@asproni-fermi.edu.it>
2025-11-15 11:47:46 +0100

    Commit automatico 15/11/2025 11:47:46,91

--- Informatica/Programmazione/Java/Classe Casa/src/casa/Casa.java
+++ Informatica/Programmazione/Java/Classe Casa/src/casa/Casa.java
@@ -1,4 +1,21 @@
-
+package  casa;
 public class Casa {
+    private String indirizzo;
+    private int numeroStanze;
+    private double superficie;
+    private double prezzo;
+    private boolean conGiardino;
+    private int annoCostruzione;
+
+
+    // Costruttore principale
+    public Casa(String indirizzo, int numeroStanze, double superficie, double prezzo, boolean conGiardino, int annoCostruzione) {
+        this.indirizzo = indirizzo;
+        this.numeroStanze = numeroStanze;
+        this.superficie = superficie;
+        this.prezzo = prezzo;
+        this.conGiardino = conGiardino;
+        this.annoCostruzione = annoCostruzione;
+    }
 
 }
