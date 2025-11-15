0cbae84b4ecf0c26df4466c6344477d4bc511d5c
MatteoPorcu08 <porcumatteo@asproni-fermi.edu.it>
2025-11-15 11:52:49 +0100

    Commit automatico 15/11/2025 11:52:49,02

--- Informatica/Programmazione/Java/Classe Casa/src/casa/Casa.java
+++ Informatica/Programmazione/Java/Classe Casa/src/casa/Casa.java
@@ -18,4 +18,74 @@ public class Casa {
         this.annoCostruzione = annoCostruzione;
     }
 
+    // Costruttore di copia
+    public Casa(Casa altraCasa) {
+        this.indirizzo = altraCasa.indirizzo;
+        this.numeroStanze = altraCasa.numeroStanze;
+        this.superficie = altraCasa.superficie;
+        this.prezzo = altraCasa.prezzo;
+        this.conGiardino = altraCasa.conGiardino;
+        this.annoCostruzione = altraCasa.annoCostruzione;
+    }
+
+    // Metodi getter
+    public String getIndirizzo() {
+        return indirizzo;
+    }
+    public int getNumeroStanze() {
+        return numeroStanze;
+    }
+    public double getSuperficie() {
+        return superficie;
+    }
+    public double getPrezzo() {
+        return prezzo;
+    }
+    public boolean ConGiardino() {
+        return conGiardino;
+    }
+    public int getAnnoCostruzione() {
+        return annoCostruzione;
+    }
+    
+    //Metodo setter
+    public void setIndirizzo(String indirizzo) {
+        this.indirizzo = indirizzo;
+    }
+    public void setNumeroStanze(int numeroStanze) {
+        this.numeroStanze = numeroStanze;
+    }
+    public void setSuperficie(double superficie) {
+        this.superficie = superficie;
+    }
+    public void setPrezzo(double prezzo) {
+        this.prezzo = prezzo;
+    }
+    public void setConGiardino(boolean conGiardino) {
+        this.conGiardino = conGiardino;
+    }
+    public void setAnnoCostruzione(int annoCostruzione) {
+        this.annoCostruzione = annoCostruzione;
+    }
+
+    //metodi di confrontoù
+    //confornto della superficie
+    public boolean haSuperficieMaggioreDi(Casa altraCasa) {
+        if (this.superficie > altraCasa.superficie) {
+            return ("La casa in " + this.indirizzo + " ha una superficie maggiore di " + altraCasa.indirizzo);
+        } else {
+            return false;
+        }
+    }
+    //confronto del prezzo
+    public boolean haPrezzoMinoreDi(Casa altraCasa) {
+        return this.prezzo < altraCasa.prezzo;
+    }
+
+
+
+
+
+
+
 }
