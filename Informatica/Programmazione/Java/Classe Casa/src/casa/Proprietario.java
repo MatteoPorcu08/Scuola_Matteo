8b77fa45d1e572e4c23d4cfbfa9add4ddd379af4
MatteoPorcu08 <porcumatteo@asproni-fermi.edu.it>
2025-11-17 08:58:51 +0100

    Commit automatico 17/11/2025  8:58:50,97

--- Informatica/Programmazione/Java/Classe Casa/src/casa/Proprietario.java
+++ Informatica/Programmazione/Java/Classe Casa/src/casa/Proprietario.java
@@ -5,6 +5,33 @@ public class Proprietario {
     private String codiceFiscale;
     public Casa home;
 
+    //Costruttore Principale
+    public Proprietario(String nome, String cognome, String codiceFiscale, Casa home) {
+        this.nome = nome;
+        this.cognome = cognome;
+        this.codiceFiscale = codiceFiscale;
+        this.home = home;
+    }
+
+    //Costruttore Di Copia
+    public Proprietario(Proprietario altroProprietario) {
+        this.nome = altroProprietario.nome;
+        this.cognome = altroProprietario.cognome;
+        this.codiceFiscale = altroProprietario.codiceFiscale;
+        this.home = altroProprietario.home;
+    }
+
+    //Metodo Getter
+    public String getNome() {
+        return nome;
+    }
+    public String getCognome() {
+        return cognome;
+    }
+    public String getCodiceFiscale() {
+        return codiceFiscale;
+    }
+
 
 
 }
