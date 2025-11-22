f421e2dce0f198a2f6a00021a78845156eb3fa3d
MatteoPorcu08 <porcumatteo@asproni-fermi.edu.it>
2025-11-22 11:47:01 +0100

    Commit automatico 22/11/2025 11:47:01,69

---
+++ Informatica/Programmazione/Java/Anagrafica copy/src/elenco/Persona.java
@@ -0,0 +1,61 @@
+package elenco;
+
+public class Persona {
+
+    // ATTRIBUTI: nome, cognome, età, altezza
+    private String nome;
+    private String cognome;
+    private int eta;
+    private float  altezza;
+
+    // METODI
+
+
+
+    // COSTRUTTORE
+    public Persona (String nome, String cognome, int eta, float altezza) {
+        this.nome = nome;
+        this.cognome = cognome;
+        this.eta = eta;
+        this.altezza = altezza;
+
+    }
+    /* SETTER:Metodi che assegnano i valori ai singoli parametri
+        Possono essere pubblici (attributi dinamici) o privato (attributi statici)*/
+
+        private void setNome (String nome) {
+            this.nome = nome;
+        }
+        private void setCognome (String cognome) {
+            this.cognome = cognome;
+        }
+        public void setEta (int eta) {
+            this.eta = eta;
+        }
+        public void setAltezza (float altezza) {
+            this.altezza = altezza;
+        }
+    
+    /* GETTER: Metodi che restituiscono i valori dei singoli parametri
+        Sono sempre pubblici perchè devono essere utilizati anche all'esterno della classe */
+
+        public String getNome () {
+            return this.nome;
+        }
+        public String getCognome () {
+            return this.cognome;
+        }
+        public int getEta () {
+            return this.eta;
+        }
+        public float getAltezza () {
+            return this.altezza;
+        }
+
+        //metodo toString:serve per restituire i valori degli attributi
+        public String toString () {
+            return "Nome=" + this.nome + "\nCognome=" +this.cognome + "\nEtà=" + this.eta + "\nAltezza=" + this.altezza;
+        }
+
+    
+}
--- Informatica/Programmazione/Java/Computer Scanner/src/elaboratori/Main.java
+++ Informatica/Programmazione/Java/Computer Scanner/src/elaboratori/Main.java
@@ -4,10 +4,56 @@ public class Main {
     public static void main(String[] args) {
         Scanner s= new Scanner(System.in);
         //creo due oggetti computer
-        computer pc1 = new comeputer;
-        computer pc2 = new comeputer;
-        System 
         
+        Computer c1;
+        System.out.println("Ciao, questo programma serve per catalogare i computer.");
+        System.out.println("Inserisci la marca del tuo pc: ");
+        String marca;
+        marca=s.nextLine();
+        System.out.println("Inserisci il modello del tuo pc: ");
+        String modello;
+        modello=s.nextLine();
+        System.out.println("Inserisci il prezzo del tuo pc: ");
+        double prezzo;
+        prezzo=s.nextDouble();
+        s.nextLine();
+        System.out.println("Inserisci la CPU del tuo pc: ");
+        String cpu;
+        cpu=s.nextLine();
+        System.out.println("Inserisci la quantità di RAM del tuo pc: ");
+        int ram;
+        ram=s.nextInt();
+        s.nextLine();
+        System.out.println("Inserisci la quantità di memoria del tuo pc: ");
+        int storage;
+        storage=s.nextInt();
+        s.nextLine();
+
+        Computer pc1 = new Computer(marca, modello, prezzo, cpu, ram, storage);
+
+        Computer c2;
+        System.out.println("Inserisci la marca del secondo pc: ");
+        String marca2;
+        marca2=s.nextLine();
+        System.out.println("Inserisci il modello del secondo pc: ");
+        String modello2;
+        modello2=s.nextLine();
+        System.out.println("Inserisci il prezzo del secondo pc: ");
+        double prezzo2;
+        prezzo2=s.nextDouble();
+        s.nextLine();
+        System.out.println("Inserisci la CPU del secondo pc: ");
+        String cpu2;
+        cpu2=s.nextLine();
+        System.out.println("Inserisci la quantità di RAM del secondo pc: ");
+        int ram2;
+        ram2=s.nextInt();
+        s.nextLine();
+        System.out.println("Inserisci la quantità di memoria del secondo pc: ");
+        int storage2;
+        storage2=s.nextInt();
+        s.nextLine();
+        Computer pc2 = new Computer(marca2, modello2, prezzo2, cpu2, ram2, storage2);
         
         System.out.println("Benvenuto! Questo programma serve per conforntare due computer."); //Messaggio di Benvenuto
 
