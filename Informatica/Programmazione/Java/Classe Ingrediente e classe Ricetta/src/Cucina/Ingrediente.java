0665f9839016c7600c7f37eee61bcac3366c9110
MatteoPorcu08 <porcumatteo@asproni-fermi.edu.it>
2025-11-24 09:00:50 +0100

    Commit automatico 24/11/2025  9:00:50,68

--- Informatica/Programmazione/Java/Classe Ingrediente e classe Ricetta/src/Cucina/Ingrediente.java
+++ Informatica/Programmazione/Java/Classe Ingrediente e classe Ricetta/src/Cucina/Ingrediente.java
@@ -1,4 +1,21 @@
 package Cucina;
 public class Ingrediente {
+    private String nome;
+    private double quantita; // in grammi
+    private String unita;
+    private double caloriePerUnita;
+    private double costoPerUnita;
+    private boolean vegetariano;
 
+    //Costruttore principale
+    public Ingrediente(String nome, double quantita, String unita, double caloriePerUnita, double costoPerUnita, boolean vegetariano) {
+        this.nome = nome;
+        this.quantita = quantita;
+        this.unita = unita;
+        this.caloriePerUnita = caloriePerUnita;
+        this.costoPerUnita = costoPerUnita;
+        this.vegetariano = vegetariano;
+
+
+}
 }
