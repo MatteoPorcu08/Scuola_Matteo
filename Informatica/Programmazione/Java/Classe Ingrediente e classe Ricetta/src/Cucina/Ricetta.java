8413f43d940cca317caca6fba0cc601dba34da27
MatteoPorcu08 <porcumatteo@asproni-fermi.edu.it>
2025-11-24 09:15:56 +0100

    Commit automatico 24/11/2025  9:15:56,78

--- Informatica/Programmazione/Java/Classe Ingrediente e classe Ricetta/src/Cucina/Ricetta.java
+++ Informatica/Programmazione/Java/Classe Ingrediente e classe Ricetta/src/Cucina/Ricetta.java
@@ -6,5 +6,76 @@ public class Ricetta {
     private int porzioni;
     private Ingrediente ingredienti1, ingredienti2, ingredienti3;
 
+    //Costruttore principale
+    public Ricetta(String nome, int tempoPreparazione, String difficolta, int porzioni, Ingrediente ingredienti1, Ingrediente ingredienti2, Ingrediente ingredienti3) {
+        this.nome = nome;
+        this.tempoPreparazione = tempoPreparazione;
+        this.difficolta = difficolta;
+        this.porzioni = porzioni;
+        this.ingredienti1 = new Ingrediente(ingredienti1);
+        this.ingredienti2 = new Ingrediente(ingredienti2);
+        this.ingredienti3 = new Ingrediente(ingredienti3);
+    }
+
+    //Costruttore di copia
+    public Ricetta(Ricetta altra) {
+        this.nome = altra.nome;
+        this.tempoPreparazione = altra.tempoPreparazione;
+        this.difficolta = altra.difficolta;
+        this.porzioni = altra.porzioni;
+        this.ingredienti1 = new Ingrediente(altra.ingredienti1);
+        this.ingredienti2 = new Ingrediente(altra.ingredienti2);
+        this.ingredienti3 = new Ingrediente(altra.ingredienti3);
+    }
+
+    //Metodi getter
+    public String getNome() {
+        return nome;
+    }
+    public int getTempoPreparazione() {
+        return tempoPreparazione;
+    }
+    public String getDifficolta() {
+        return difficolta;
+    }
+    public int getPorzioni() {
+        return porzioni;
+    }
+    public Ingrediente getIngredienti1() {
+        return new Ingrediente(ingredienti1);
+    }
+    public Ingrediente getIngredienti2() {
+        return new Ingrediente(ingredienti2);
+    }
+    public Ingrediente getIngredienti3() {
+        return new Ingrediente(ingredienti3);
+    }
+
+    //Metodi setter
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    public void setTempoPreparazione(int tempoPreparazione) {
+        this.tempoPreparazione = tempoPreparazione;
+    }
+    public void setDifficolta(String difficolta) {
+        this.difficolta = difficolta;
+    }
+    public void setPorzioni(int porzioni) {
+        this.porzioni = porzioni;
+    }
+    public void setIngredienti1(Ingrediente ingredienti1) {
+        this.ingredienti1 = new Ingrediente(ingredienti1);
+    }
+    public void setIngredienti2(Ingrediente ingredienti2) {
+        this.ingredienti2 = new Ingrediente(ingredienti2);
+    }
+    public void setIngredienti3(Ingrediente ingredienti3) {
+        this.ingredienti3 = new Ingrediente(ingredienti3);
+    }
+
+    //Metodo toString
+    @Override
+
 
 }
