6b69971da79a273130b91d50a0c99598e3c6913c
MatteoPorcu08 <porcumatteo@asproni-fermi.edu.it>
2025-11-24 19:23:00 +0100

    Commit automatico 24/11/2025 19:23:00,79

--- Informatica/Programmazione/Java/Classe Ingrediente e classe Ricetta/src/Cucina/Main.java
+++ Informatica/Programmazione/Java/Classe Ingrediente e classe Ricetta/src/Cucina/Main.java
@@ -6,5 +6,9 @@ public class Main {
     Scanner s = new Scanner(System.in);
 
     System.out.println("Ciao! Benvenuto nella cucina");
+    System.out.println("Inserisci il nome della ricetta:");
+    String nomeRicetta = s.nextLine();
+    Ricetta ricetta = new Ricetta(nomeRicetta);
+    boolean continua = true;
 
 }
