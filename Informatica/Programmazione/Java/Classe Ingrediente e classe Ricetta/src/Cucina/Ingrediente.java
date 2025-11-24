8be174c2af2d23a44b3d6a98c5c366396ec4e532
MatteoPorcu08 <porcumatteo@asproni-fermi.edu.it>
2025-11-24 09:05:52 +0100

    Commit automatico 24/11/2025  9:05:52,74

--- Informatica/Programmazione/Java/Classe Ingrediente e classe Ricetta/src/Cucina/Ingrediente.java
+++ Informatica/Programmazione/Java/Classe Ingrediente e classe Ricetta/src/Cucina/Ingrediente.java
@@ -15,7 +15,61 @@ public class Ingrediente {
         this.caloriePerUnita = caloriePerUnita;
         this.costoPerUnita = costoPerUnita;
         this.vegetariano = vegetariano;
+    }
+    
+    //Costruttore di copia
+    public Ingrediente(Ingrediente altro) {
+        this.nome = altro.nome;
+        this.quantita = altro.quantita;
+        this.unita = altro.unita;
+        this.caloriePerUnita = altro.caloriePerUnita;
+        this.costoPerUnita = altro.costoPerUnita;
+        this.vegetariano = altro.vegetariano;
+    }
 
+    //Metodi getter
+    public String getNome() {
+        return nome;
+    }
+    public double getQuantita() {
+        return quantita;
+    }
+    public String getUnita() {
+        return unita;
+    }
+    public double getCaloriePerUnita() {
+        return caloriePerUnita;
+    }
+    public double getCostoPerUnita() {
+        return costoPerUnita;
+    }
+    public boolean isVegetariano() {
+        return vegetariano;
+    }
 
-}
+    //Metodi setter
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    public void setQuantita(double quantita) {
+        this.quantita = quantita;
+    }
+    public void setUnita(String unita) {
+        this.unita = unita;
+    }
+    public void setCaloriePerUnita(double caloriePerUnita) {
+        this.caloriePerUnita = caloriePerUnita;
+    }
+    public void setCostoPerUnita(double costoPerUnita) {
+        this.costoPerUnita = costoPerUnita;
+    }
+    public void setVegetariano(boolean vegetariano) {
+        this.vegetariano = vegetariano;
+    }
+
+    //Metodo toString
+    @Override
+    public String toString() {
+        return "Ingrediente{" +
+                "nome='" + nome + '\'' +"\n quantita=" + quantita +", unita='" + unita + '\'' +", caloriePerUnita=" + caloriePerUnita +", costoPerUnita=" + costoPerUnita +", vegetariano=" + vegetariano +'}';
 }
