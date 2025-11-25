68c750bfcae02fcf923b357d6e93cf5dc0523050
MatteoPorcu08 <porcumatteo@asproni-fermi.edu.it>
2025-11-25 12:59:37 +0100

    Commit automatico 25/11/2025 12:59:37,76

--- Informatica/Programmazione/Java/Classe Ingrediente e classe Ricetta/src/Cucina/Main.java
+++ Informatica/Programmazione/Java/Classe Ingrediente e classe Ricetta/src/Cucina/Main.java
@@ -2,12 +2,16 @@ package Cucina;
 import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
+        Ingrediente 
         Scanner s = new Scanner(System.in);
         System.out.println("Inserisci il nome della ricetta:");
         String nome = s.nextLine();
         System.out.println("Inserisci il tempo di preparazione:");
-        int tempoPreparazione = s.nextLine();
+        int tempoPreparazione = s.nextInt();
         System.out.println("Inserisci la difficolta:");
+        int difficolta = s.nextInt();
+        System.out.println("Inserisci le porzioni desiderate:");
+        int porzioni = s.nextInt();
 
 
         s.close();
