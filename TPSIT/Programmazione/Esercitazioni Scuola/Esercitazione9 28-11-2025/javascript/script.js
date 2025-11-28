f6353f852fd2eb21eea86621be653dbf24214e4a
MatteoPorcu08 <porcumatteo@asproni-fermi.edu.it>
2025-11-28 12:19:24 +0100

    Commit automatico 28/11/2025 12:19:24,78

--- TPSIT/Programmazione/Esercitazioni Scuola/Esercitazione9 28-11-2025/javascript/script.js
+++ TPSIT/Programmazione/Esercitazioni Scuola/Esercitazione9 28-11-2025/javascript/script.js
@@ -1,12 +1,12 @@
-function aggiungi() {
+function aggiungi(){
     let bevanda = document.getElementById("inputbevanda").value;
-    let quantita = Number(document.getElementById("inputquantita").value);
+    let quantita = document.getElementById("inputquantita").value*1;
 
-    if (quantita > 0) {
-        document.getElementById("lista").innerHTML += "<br> - " + quantita + "x " + bevanda;
+    if(quantita > 0){
+        document.getElementById("lista").innerHTML =
+        document.getElementById("lista").innerHTML + "<br>-" + bevanda+ " x " +quantita;
         document.getElementById("inputbevanda").value = "";
-        document.getElementById("inputquantita").value = "";
-    } else {
+    } else{
         alert("Errore! Inserire un numero positivo.");
     }
 }
