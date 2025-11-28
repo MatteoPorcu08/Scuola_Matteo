3b59df90c9ef20abe2d32452b383d2948bd82faf
MatteoPorcu08 <porcumatteo@asproni-fermi.edu.it>
2025-11-28 13:14:45 +0100

    Commit automatico 28/11/2025 13:14:45,74

--- TPSIT/Programmazione/Esercitazioni Scuola/Esercitazione9 28-11-2025/javascript/script.js
+++ TPSIT/Programmazione/Esercitazioni Scuola/Esercitazione9 28-11-2025/javascript/script.js
@@ -1,4 +1,5 @@
 let totale = 0;
+let quantitaTotale = 0;
 function aggiungi() {
     let bevanda = document.getElementById("inputbevanda").value;
     let quantita = document.getElementById("inputquantita").value * 1;
@@ -6,28 +7,30 @@ function aggiungi() {
     if (quantita > 0) {
         document.getElementById("lista").innerHTML += "<br>-" + bevanda + " x " + quantita;
         document.getElementById("inputbevanda").value = "";
+        document.getElementById("inputquantita").value = "";
+        const prezzo = 1.20;
+        let prezzoBevanda = quantita * prezzo;
+        totale += prezzoBevanda;
+        document.getElementById("prezzo").innerHTML = "Totale: " + totale + " Eur";
+        quantitaTotale += quantita;
     } else {
         alert("Errore! Inserire un numero positivo.");
-        return;
     }
 
-    const prezzo = 1.20;
-    let prezzoBevanda = quantita * prezzo;
-    totale += prezzoBevanda;
-    document.getElementById("prezzo").innerHTML = "Totale: " + totale + " Eur";
+
 }
 
 function conferma(){
     let conferma=prompt("Vuoi confermare l’ordine? (s/n)");
-    let quantita = document.getElementById("inputquantita").value * 1;
     if (conferma == "s" || conferma == "S" ){
-        alert("Il numero dei prodotto selezionati e:" +quantita+ "\n" +
-            "Il prezzo totale e:" +totale)
+        alert("Il numero dei prodotto selezionati e: " +quantitaTotale+ "\n" +
+            "Il prezzo totale e: " +totale)
     } else if(conferma == "n"|| conferma == "N"){
         alert("Ordine Anullato!");
     }
 
     document.getElementById("lista").innerHTML = "";
-    document.getElementById("totale").innerHTML = "Totale:";
+    document.getElementById("prezzo").innerHTML = "";
     totale= 0 ;
+    quantitaTotale=0;
 }
