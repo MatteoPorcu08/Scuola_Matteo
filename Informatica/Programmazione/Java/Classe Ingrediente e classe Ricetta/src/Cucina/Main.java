56d5ea2dc9b40697fa14360b11b4993f1716c9ba
MatteoPorcu08 <porcumatteo@asproni-fermi.edu.it>
2025-11-28 15:43:33 +0100

    Commit automatico 28/11/2025 15:43:33,70

--- Informatica/Programmazione/Java/Classe Ingrediente e classe Ricetta/src/Cucina/Main.java
+++ Informatica/Programmazione/Java/Classe Ingrediente e classe Ricetta/src/Cucina/Main.java
@@ -1,37 +1,103 @@
 package Cucina;
+
 import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
 
         Scanner s = new Scanner(System.in);
 
-        //chiedo l'input all'utente
-        System.out.println("Inserisci il nome della ricetta:");
-        String nome = s.nextLine();
+        //Creazione Ingredienti
+        System.out.println("--- Inserimento ingrediente 1 ---");
+        Ingrediente ingr1 = creaIngrediente(s);
 
-        System.out.println("Inserisci il tempo di preparazione (minuti):");
-        int tempoPreparazione = s.nextInt();
+        System.out.println("\n--- Inserimento ingrediente 2 ---");
+        Ingrediente ingr2 = creaIngrediente(s);
 
-        System.out.println("Inserisci il numero di porzioni:");
-        int porzioni = s.nextInt();
-
-        //Ingredienti manuali
-        Ingrediente ingr1 = new Ingrediente("Pasta", 200, "g", 350, 0.50, true);
-        Ingrediente ingr2 = new Ingrediente("Pomodoro", 150, "g", 40, 0.30, true);
-        Ingrediente ingr3 = new Ingrediente("Olio", 20, "g", 900, 1.00, true);
+        System.out.println("\n--- Inserimento ingrediente 3 ---");
+        Ingrediente ingr3 = creaIngrediente(s);
 
         //Creazione ricetta
-        Ricetta ricetta = new Ricetta(nome, tempoPreparazione, porzioni, ingr1, ingr2, ingr3);
+        Ricetta r1 = new Ricetta("Pasta alla Carbonara", 20, "Media", 2, ingr1, ingr2, ingr3);
 
-        //Stampo la ricetta
-        System.out.println("\n--- RICETTA COMPLETA ---");
-        System.out.println(ricetta);
+        boolean continua = true;
+        while (continua) {
+            System.out.println("\n--- MENU ---");
+            System.out.println("1. Stampa ricetta");
+            System.out.println("2. Scala dosi");
+            System.out.println("3. Calcola calorie totali");
+            System.out.println("4. Stima costo totale");
+            System.out.println("5. Verifica se vegetariana");
+            System.out.println("6. Sostituisci ingrediente");
+            System.out.println("0. Esci");
+            System.out.print("Scelta: ");
+            int scelta = s.nextInt();
+            s.nextLine();
 
-        // Stampo gli ingredienti
-        System.out.println("\n--- INGREDIENTI ---");
-        System.out.println("Ingrediente 1:\n" + ingr1);
-        System.out.println("Ingrediente 2:\n" + ingr2);
-        System.out.println("Ingrediente 3:\n" + ingr3);
+            switch (scelta) {
+                case 1:
+                    System.out.println("\n" + r1);
+                    break;
+                case 2:
+                    System.out.print("Inserisci nuove porzioni: ");
+                    int nuovePorzioni = s.nextInt();
+                    s.nextLine();
+                    r1.scalaDosi(nuovePorzioni);
+                    System.out.println("Porzioni aggiornate!");
+                    break;
+                case 3:
+                    System.out.println("Calorie totali: " + r1.calcolaCalorieTotali());
+                    break;
+                case 4:
+                    System.out.println("Costo totale: " + r1.stimaCostoTotale() + "€");
+                    break;
+                case 5:
+                    if (r1.isVegetariana()) {
+                        System.out.println("La ricetta è vegetariana.");
+                    } else {
+                        System.out.println("La ricetta NON è vegetariana.");
+                    }
+                    break;
+                case 6:
+                    System.out.print("Numero ingrediente da sostituire (1-3): ");
+                    int numIng = s.nextInt();
+                    s.nextLine();
+                    System.out.println("Inserisci il nuovo ingrediente:");
+                    Ingrediente nuovoIng = creaIngrediente(s);
+                    if (r1.sostituisciIngrediente(numIng, nuovoIng)) {
+                        System.out.println("Ingrediente sostituito!");
+                    } else {
+                        System.out.println("Numero ingrediente non valido!");
+                    }
+                    break;
+                case 0:
+                    continua = false;
+                    System.out.println("Arrivederci!");
+                    break;
+                default:
+                    System.out.println("Scelta non valida!");
+            }
+        }
+
         s.close();
+    }
+
+    // Metodo per creare un ingrediente tramite Scanner
+    private static Ingrediente creaIngrediente(Scanner s) {
+        System.out.print("Nome: ");
+        String nome = s.nextLine();
+        System.out.print("Quantità: ");
+        double quant = s.nextDouble();
+        s.nextLine();
+        System.out.print("Unità di misura: ");
+        String unita = s.nextLine();
+        System.out.print("Calorie per unità: ");
+        double cal = s.nextDouble();
+        System.out.print("Costo per unità: ");
+        double costo = s.nextDouble();
+        System.out.print("Vegetariano? (true/false): ");
+        boolean veg = s.nextBoolean();
+        s.nextLine();
+        return new Ingrediente(nome, quant, unita, cal, costo, veg);
     }
 }
