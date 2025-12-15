216056b39750eebc771855a0daf9c572d532fd28
MatteoPorcu08 <porcumatteo@asproni-fermi.edu.it>
2025-12-15 10:19:10 +0100

    Commit automatico 15/12/2025 10:19:10,74

--- Informatica/Programmazione/Java/Classi Wrapper/src/wrapper/Main.java
+++ Informatica/Programmazione/Java/Classi Wrapper/src/wrapper/Main.java
@@ -1,4 +1,12 @@
 package  wrapper;
 public class Main {
-    
+    Integer num1= new Integer(10);
+    Integer num2= new Integer(20);
+    public void stampa(){
+        System.out.println("Num1: "+num1);
+        System.out.println("Num2: "+num2);
+    }
+
+    //PARTE 2: Conversione da Stringa a Numero
+
 }
