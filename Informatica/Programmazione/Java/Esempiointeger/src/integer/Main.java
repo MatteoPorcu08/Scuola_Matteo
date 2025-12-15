9068edd0c9ebc5c77f00fd3cf706cc362db62929
MatteoPorcu08 <porcumatteo@asproni-fermi.edu.it>
2025-12-15 09:29:33 +0100

    Commit automatico 15/12/2025  9:29:33,84

--- Informatica/Programmazione/Java/Esempiointeger/src/integer/Main.java
+++ Informatica/Programmazione/Java/Esempiointeger/src/integer/Main.java
@@ -1,9 +1,10 @@
+package integer;
 public class Main {
     public static void main(String[] args) {
         int n;
-        Integer numero = new Integer(10);
+        int numero = 10;
         n=100;
-        n=n*numero.intValue();
+        n=n*numero;
         System.out.println("Il valore di n e': " + n);
     }
 }
