a8a2f00d2995c3abea6102f724350cb4cfc95b24
MatteoPorcu08 <porcumatteo@asproni-fermi.edu.it>
2026-01-09 12:52:02 +0100

    Commit automatico 09/01/2026 12:52:02,85

--- TPSIT/Programmazione/Esercitazioni Scuola/Esercitazione 09-01-2025 Gestione Carriera/javascript/script.js
+++ TPSIT/Programmazione/Esercitazioni Scuola/Esercitazione 09-01-2025 Gestione Carriera/javascript/script.js
@@ -1,70 +1,70 @@
-function AggiungiEsameSvolgere(){
+function AggiungiEsameSvolgere() {
 
-    let nomeesame = prompt("Aggiungi nome esame");
-    document.getElementById("nomeesami").innerHTML = document.getElementById("nomeesami").innerHTML +"<br>"+ nomeesame;
+    let nome = prompt("Inserisci il nome dell'esame da svolgere");
 
-    document.getElementById("numeroesamisvolgere").innerHTML = document.getElementById("numeroesamisvolgere").innerHTML*1+1;
+    if (nome != null && nome != "") {
 
+        document.getElementById("tabellaSvolgere").innerHTML +=
+            "<tr><td>" + nome + "</td></tr>";
 
-    document.getElementById("esameaggiuntosvolgere").innerHTML = "Esame aggiunto correttamente!" ;
+        document.getElementById("numeroesamisvolgere").innerHTML =
+            document.getElementById("numeroesamisvolgere").innerHTML * 1 + 1;
 
-    
-
-
-}
-
-function AggiungiEsameSvolto(){
-    let esamesvolto = prompt("Aggiungi esame svolto");
-    document.getElementById("esamisvolti").innerHTML = document.getElementById("esamisvolti").innerHTML +"<br>"+ esamesvolto;
-    
-    document.getElementById("numeroesamisvolti").innerHTML = document.getElementById("numeroesamisvolti").innerHTML*1+1;
-
-    document.getElementById("esameaggiuntosvolto").innerHTML = "Bravo hai terminato con "+ esamesvolto+"" ;
-
-    
-
-
-}
-
-function VerificaCarriera(){
-    let NumeroEsamiSvolti = Number(document.getElementById("numeroesamisvolti").innerHTML);
-    let NumeroEsamiSvolgere = Number(document.getElementById("numeroesamisvolgere").innerHTML);
-
-    let Sommapagare = Number(document.getElementById("sommapagare").innerHTML)
-   
-    if(NumeroEsamiSvolti >= NumeroEsamiSvolgere && Sommapagare == 0){
-
-            alert("Bravo,hai terminato con successo");
-
-    }else if(NumeroEsamiSvolti >= NumeroEsamiSvolgere && Sommapagare > 0){
-
-            alert("Hai terminato gli esami ma hai ancora " + Sommapagare + "");
-    }else{
-
-            alert("Hai ancora esami da svolgere!");
-
+        document.getElementById("esameaggiuntosvolgere").innerHTML =
+            "Esame aggiunto correttamente!";
     }
-    
-    
 }
 
-function Paga(){
-    let paga = prompt("Quanto vuoi Pagare?")*1;
-    let valoredapagare = document.getElementById("sommapagare").innerHTML*1;
+function AggiungiEsameSvolto() {
 
-    
-    while(paga > valoredapagare || paga < 0){
-        alert("Hai inserito un importo negativo o troppo grande , RIPROVA!");
-        paga = prompt("Quanto vuoi Pagare?")*1;
+    let nome = prompt("Inserisci il nome dell'esame svolto");
+
+    if (nome != null && nome != "") {
+
+        document.getElementById("tabellaSvolti").innerHTML +=
+            "<tr><td>" + nome + "</td></tr>";
+
+        document.getElementById("numeroesamisvolti").innerHTML =
+            document.getElementById("numeroesamisvolti").innerHTML * 1 + 1;
+
+        document.getElementById("esameaggiuntosvolto").innerHTML =
+            "Bravo hai terminato con " + nome + "!";
     }
-    
-     
-      document.getElementById("sommapagare").innerHTML = valoredapagare - paga;
-    
-
-   
-
-   
-   
 }
 
+function VerificaCarriera() {
+
+    let svolti =
+        document.getElementById("numeroesamisvolti").innerHTML * 1;
+
+    let daSvolgere =
+        document.getElementById("numeroesamisvolgere").innerHTML * 1;
+
+    let somma =
+        document.getElementById("sommapagare").innerHTML * 1;
+
+    if (svolti >= daSvolgere && somma == 0) {
+        alert("Bravo, hai terminato con successo");
+    }
+    else if (svolti >= daSvolgere && somma > 0) {
+        alert("Hai terminato gli esami ma devi pagare " + somma + " EUR");
+    }
+    else {
+        alert("Hai ancora esami da svolgere!");
+    }
+}
+
+function Paga() {
+
+    let paga = prompt("Quanto vuoi pagare?") * 1;
+    let totale =
+        document.getElementById("sommapagare").innerHTML * 1;
+
+    while (paga <= 0 || paga > totale) {
+        alert("Errore! Importo non valido");
+        paga = prompt("Quanto vuoi pagare?") * 1;
+    }
+
+    document.getElementById("sommapagare").innerHTML =
+        totale - paga;
+}
