199a960e06941aa540faac476beb0d71db6c5bff
MatteoPorcu08 <porcumatteo@asproni-fermi.edu.it>
2026-01-10 12:05:29 +0100

    Commit automatico 10/01/2026 12:05:29,73

--- TPSIT/Programmazione/Esercitazioni Scuola/Esercitazione 09-01-2026 MatteoPorcu E-Gym copy/javascript/script.js
+++ TPSIT/Programmazione/Esercitazioni Scuola/Esercitazione 09-01-2026 MatteoPorcu E-Gym copy/javascript/script.js
@@ -1,41 +1,52 @@
-let totale=0;
-function aggiungiCorso(){
-    let corso= document.getElementById("nomecorso").value;
-    let istruttore= document.getElementById("nomeistruttore").value;
+let totale = 0;
 
-    document.getElementById("listacorso").innerHTML += "<br>" + corso;
-    document.getElementById("listaistruttore").innerHTML += "<br>" + istruttore;
-        document.getElementById("nomecorso").value = "";
-        document.getElementById("nomeistruttore").value = "";
+function aggiungiCorso() {
+    let corso = document.getElementById("nomecorso").value;
+    let istruttore = document.getElementById("nomeistruttore").value;
+
+    document.getElementById("tabella").innerHTML +=
+        "<tr><td>" + corso + "</td><td>" + istruttore + "</td></tr>";
+
+    document.getElementById("messaggio").innerHTML =
+        "Corso aggiunto correttamente!";
+
+    document.getElementById("nomecorso").value = "";
+    document.getElementById("nomeistruttore").value = "";
 }
 
-function elimina(){
-    if(document.getElementById("listacorso").innerHTML == "" && document.getElementById("listaistruttore").innerHTML == "" ){
+function elimina() {
+    if (document.getElementById("tabella").innerHTML == "") {
         alert("Non ci sono corsi da eliminare!");
-    } else{
-        document.getElementById("listacorso").innerHTML = "";
-        document.getElementById("listaistruttore").innerHTML = "";
+    } else {
+        document.getElementById("tabella").innerHTML = "";
         alert("Corsi eliminati!");
     }
 }
 
-function aggiungiCosti(){
+function aggiungiCosti() {
     let corso;
     let prezzo;
-    if(totale>700){
-            alert("Attenzione il totale ha superato 700EUR");
-        }else{
-            corso=prompt("Inserisci il nome del corso:");
-            do{
-            prezzo= prompt("Inserisci il prezzo del corso:")*1;
-        if(prezzo<=0){
-            alert("Errore!Il prezzo deve essere maggiore di 0");
-        }
-    }while(prezzo<=0);
-    
-    document.getElementById("prezzo").innerHTML += "<br>" + corso + ":" + prezzo + " EUR";
 
-    totale += prezzo;
-    document.getElementById("totaleinclusiva").innerHTML =  + totale ;
+    if (totale > 700) {
+        alert("Errore: il totale ha già superato 700 EUR");
+    } else {
+        corso = prompt("Inserisci il nome del corso:");
+
+        do {
+            prezzo = prompt("Inserisci il prezzo del corso:") * 1;
+            if (prezzo <= 0) {
+                alert("Errore! Il prezzo deve essere maggiore di 0");
+            }
+        } while (prezzo <= 0);
+
+        if (totale + prezzo > 700) {
+            alert("Attenzione: il totale supererà 700 EUR");
         }
+
+        document.getElementById("prezzo").innerHTML +=
+            "<br>" + corso + ": " + prezzo + " EUR";
+
+        totale += prezzo;
+        document.getElementById("totaleinclusiva").innerHTML = totale;
+    }
 }
