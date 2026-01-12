2cd5de5cb53247ea5abddddeb85d132162f45708
MatteoPorcu08 <porcumatteo@asproni-fermi.edu.it>
2026-01-12 09:36:22 +0100

    Commit automatico 12/01/2026  9:36:22,89

--- Informatica/Programmazione/Java/Classe Evento/src/eventi/Evento.java
+++ Informatica/Programmazione/Java/Classe Evento/src/eventi/Evento.java
@@ -1,13 +1,82 @@
 package eventi;
-import java.util.Date;
-public class Evento {
-    private String nome;
-    private Date data;
 
-    public Evento(String nome, Date data, List<String> partecipanti) {
-        this.nome = nome;
-        this.data = data;
+import java.time.LocalDate;
+import java.time.temporal.ChronoUnit;
+
+public class Evento {
+
+    private LocalDate data;
+    private String descrizione;
+
+    // Costruttore principale
+    public Evento(LocalDate data, String descrizione) {
+        if (data != null && descrizione != null) {
+            this.data = data;
+            this.descrizione = descrizione;
+        }
     }
 
-    
+    // Costruttore di copia
+    public Evento(Evento e) {
+        this.data = e.data;
+        this.descrizione = e.descrizione;
+    }
+
+    // Getter e Setter
+    public LocalDate getData() {
+        return data;
+    }
+
+    public void setData(LocalDate d) {
+        if (d != null) {
+            data = d;
+        }
+    }
+
+    public String getDescrizione() {
+        return descrizione;
+    }
+
+    public void setDescrizione(String s) {
+        if (s != null) {
+            descrizione = s;
+        }
+    }
+
+    // toString
+    public String toString() {
+        return "Evento: [" + data + "] - Descrizione: " + descrizione;
+    }
+
+    // Metodi su LocalDate
+    public boolean isPast() {
+        return data.isBefore(LocalDate.now());
+    }
+
+    public boolean isFuture() {
+        return data.isAfter(LocalDate.now());
+    }
+
+    public long daysUntil() {
+        return ChronoUnit.DAYS.between(LocalDate.now(), data);
+    }
+
+    public void spostaDiGiorni(int giorni) {
+        data = data.plusDays(giorni);
+    }
+
+    // Metodi su String
+    public boolean contieneKeyword(String keyword) {
+        return descrizione.toLowerCase().contains(keyword.toLowerCase());
+    }
+
+    public int lunghezzaDescrizione() {
+        return descrizione.length();
+    }
+
+    public void aggiornaDescrizione(String nuovaDescrizione) {
+        if (nuovaDescrizione != null) {
+            descrizione = nuovaDescrizione;
+        }
+    }
 }
