76c07bd2de9c2de7d98c3a9cebc91aaa755a7304
MatteoPorcu08 <porcumatteo@asproni-fermi.edu.it>
2026-01-12 09:26:19 +0100

    Commit automatico 12/01/2026  9:26:19,78

--- Informatica/Programmazione/Java/Classe Evento/src/eventi/Evento.java
+++ Informatica/Programmazione/Java/Classe Evento/src/eventi/Evento.java
@@ -1,15 +1,12 @@
 package eventi;
 import java.util.Date;
-import java.util.List;
 public class Evento {
     private String nome;
     private Date data;
-    private List<String> partecipanti;
 
     public Evento(String nome, Date data, List<String> partecipanti) {
         this.nome = nome;
         this.data = data;
-        this.partecipanti = partecipanti;
     }
 
     
