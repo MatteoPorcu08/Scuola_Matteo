339873a2ba408970372eb9343d485b7d00a522b6
MatteoPorcu08 <porcumatteo@asproni-fermi.edu.it>
2026-01-22 10:31:25 +0100

    Commit automatico 22/01/2026 10:31:25,31

--- Informatica/Programmazione/Java/Array In Java/src/array/Main.java
+++ Informatica/Programmazione/Java/Array In Java/src/array/Main.java
@@ -2,16 +2,30 @@ package array;
 import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        Scanner s = new Scanner(System.in);
-        int v[];
-        v = new int[5];
-        for (int i = 0; i < v.length; i++) {
-            System.out.println("Inserisci il valore per l'elemento " + i);
-            v[i] = s.nextInt();
-        }
-        System.out.println("Elementi dell'array:");
-        for (int i = 0; i < v.length; i++) {
-            System.out.println(v[i]);
+        Scanner s= new Scanner(System.in);
+        int v1[];
+        int[] v2;
+        v1= new int[5];
+        v2= new int[6];
+
+        for (int i=0; i<5; i++) {
+            System.out.println("Inserisci un intero: ");
+            v1[i]=s.nextInt();
+            s.nextLine();
+    }
+
+        for (int i=0; i<5; i++) {
+            System.out.println("v1["+(i+1)+"]="+v1[i]);
+    }
+
+       //Dentro v2 inserisco i successivi di v1
+
+        for (int i=0; i<5; i++ ) {
+            v2[i]=v1[i]+1;
+    }
+        v2[5]=100;
+        for (int i=0; i<6; i++) {
+            System.out.println("v2["+(i+1)+"]="+v2[i]);
         }
     }
 }
