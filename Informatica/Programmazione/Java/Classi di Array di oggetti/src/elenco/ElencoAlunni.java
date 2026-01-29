13f10392e9223970e2502f213ac36af964fc0139
MatteoPorcu08 <porcumatteo@asproni-fermi.edu.it>
2026-01-29 09:56:31 +0100

    Commit automatico 29/01/2026  9:56:31,74

---
+++ Informatica/Programmazione/Java/Classi di Array di oggetti/src/elenco/ElencoAlunni.java
@@ -0,0 +1,12 @@
+package elenco;
+
+/**
+ * Classe di array: Serve per gestire un elenco di alunni.
+ * @author 4B INF
+ * @version 2.0
+ */
+public class ElencoAlunni {
+    //ATTRIBUTI
+    
+
+}
---
+++ Informatica/Programmazione/Java/Classi di Array di oggetti/src/elenco/Persona.java
@@ -0,0 +1,103 @@
+package elenco;
+/**
+* @author classe 4B INF
+* @version 1.2
+*/
+public class Persona {
+   //ATTRIBUTI: nome, cognome, età, altezza
+
+    private String nome;
+    private String cognome;
+    private int eta;
+    private float altezza;
+
+    //METODI
+
+    /**COSTRUTTORE
+    *
+    * @param nome nome della persona
+    * @param cognome cognome della persone
+    * @param eta età della persona
+    * @param altezza
+    */
+
+
+    public Persona(String nome, String cognome, int eta, float altezza) {
+        this.nome=nome;
+        this.cognome=cognome;
+        this.eta=eta;
+        this.altezza=altezza;
+    }
+
+    //costrutore di copia
+    public Persona(Persona P) {
+        this.nome=P.getNome();
+        this.cognome= P.getCognome();
+        this.eta= P.getEta();
+        this.altezza= P.getAltezza();
+    }
+
+    /*SETTER: metodi che assegnano i valori ai singoli parametri
+    Possono essere pubblici (attributi dinamici) o privato (attributi statici)*/
+
+    public void setNome(String nome) {
+        this.nome=nome;
+    }
+
+    private void setCognome(String cognome) {
+        this.cognome=cognome;
+    }
+
+    public void setEta(int eta) {
+        this.eta=eta;
+    }
+
+    public void setAltezza(float altezza) {
+        this.altezza=altezza;
+    }
+
+   //GETTER: metodi che servono per visualizzare i valori di ciascun attributo
+   //sono sempre pubblici perchè devono esere utilizzati anche all'esterno della classe
+
+    /**
+    * Metodo getNome
+    * @return il nome dell'oggetto
+    */
+
+    public String getNome() {
+        return this.nome;
+    }
+
+    public String getCognome() {
+        return this.cognome;
+    }
+
+    public int getEta() {
+        return this.eta;
+    }
+
+    public float getAltezza() {
+        return this.altezza;
+    }
+
+   //metodo toString: serve per restituire i valori degli attributi
+    public String toString() {
+        return "Nome = "+this.nome+"\nCognome = "+this.cognome+"\nEtà = "+this.eta+"\nAltezza = "+this.altezza;
+    }
+
+   //metodo confronta età
+    /**
+    * Metodo confrontaEta
+    * @param altraPersona è una persona di cui confrontiamo l'età
+    * @return commento relativo a chi è la persona più grande
+    */
+    public String confrontaEta(Persona altraPersona) {
+        if(this.eta>altraPersona.getEta()) {
+            return this.nome+ " è più grande di " +altraPersona.getNome();
+        }else if(this.eta==altraPersona.getEta()){
+            return this.nome + " e " + altraPersona.getNome()+ "hanno la stessa età";
+        }else {
+            return altraPersona.getNome()+ " è più grande di " + this.nome;
+        }
+    }
+}
