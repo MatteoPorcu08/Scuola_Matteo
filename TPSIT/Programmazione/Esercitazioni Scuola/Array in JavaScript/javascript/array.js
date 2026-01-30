ec9185085138ef341e64520f8154e0a4031c0034
MatteoPorcu08 <porcumatteo@asproni-fermi.edu.it>
2026-01-30 12:16:41 +0100

    Commit automatico 30/01/2026 12:16:41,82

--- TPSIT/Programmazione/Esercitazioni Scuola/Array in JavaScript/javascript/array.js
+++ TPSIT/Programmazione/Esercitazioni Scuola/Array in JavaScript/javascript/array.js
@@ -1,8 +1,14 @@
-let carrello = [];
-let i = 0;
+let carrello = []; // Array vuoto
+let i = 0; // Indice per l'inserimento degli alimenti inizializato a 0
 function aggiungi() {
     let alimento = document.getElementById("alimento").value;
-    carrello[i] = alimento;
-    i++;
-    carrello.innerHTML += `<li>${alimento}</li>`;
+    document.getElementById("alimento").value = "";
+    carrello[i] = alimento; // Aggiunge l'alimento all'array
+    i++; // Incrementa l'indice per il prossimo inserimento
+}
+function aggiornaCarrello() {
+    document.getElementById("carrello").innerHTML = ""; // Pulisce il contenuto precedente
+    for (let j = 0; j < carrello.length; j++) { // Cicla attraverso l'array
+        document.getElementById("carrello").innerHTML += carrello[j] + "<br>"; // Aggiunge ogni alimento all'elemento HTML
+    }
 }
