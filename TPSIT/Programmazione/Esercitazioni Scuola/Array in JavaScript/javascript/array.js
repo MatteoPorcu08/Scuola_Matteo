258468a6fc94a1937c1898605d5dc0e3d8c2e81a
MatteoPorcu08 <porcumatteo@asproni-fermi.edu.it>
2026-01-30 12:11:39 +0100

    Commit automatico 30/01/2026 12:11:39,82

--- TPSIT/Programmazione/Esercitazioni Scuola/Array in JavaScript/javascript/array.js
+++ TPSIT/Programmazione/Esercitazioni Scuola/Array in JavaScript/javascript/array.js
@@ -1,6 +1,8 @@
 let carrello = [];
+let i = 0;
 function aggiungi() {
     let alimento = document.getElementById("alimento").value;
-    
+    carrello[i] = alimento;
+    i++;
     carrello.innerHTML += `<li>${alimento}</li>`;
 }
