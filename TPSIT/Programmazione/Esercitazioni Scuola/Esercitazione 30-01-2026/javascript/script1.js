0eb256f2de3c9e23bf483b826b297bb997a77b7a
MatteoPorcu08 <porcumatteo@asproni-fermi.edu.it>
2026-01-30 12:41:54 +0100

    Commit automatico 30/01/2026 12:41:54,83

--- TPSIT/Programmazione/Esercitazioni Scuola/Esercitazione 30-01-2026/javascript/script1.js
+++ TPSIT/Programmazione/Esercitazioni Scuola/Esercitazione 30-01-2026/javascript/script1.js
@@ -1,18 +1,4 @@
 let i = 5; // Indice per l'inserimento del numero inizializato a 0
-function salvaNumero() {
-    let numero = document.getElementById("numeroInput").value;
-    document.getElementById("numeroInput").value = "";
-    for (let j = 0; j < i; j++) {
-        numeroSalvato = numero; // Salva il numero in una variabile
-        i++; // Incrementa l'indice per il prossimo inserimento
-    }
-}
-
-function mostraNumero() {
-    let numeroSalvato = localStorage.getItem("numeroSalvato");
-    if (numeroSalvato !== null) {
-        document.getElementById("numeroVisualizzato").innerHTML = numeroSalvato;
-    } else {
-        document.getElementById("numeroVisualizzato").innerHTML = "Nessun numero salvato.";
-    }
+function calcola() {
+    let numero = prompt("Inserisci un numero da salvare:");
 }
