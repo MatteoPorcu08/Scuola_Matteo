ef13be0742768ce814f5f2084fe60a45032dd89b
MatteoPorcu08 <porcumatteo@asproni-fermi.edu.it>
2026-01-30 13:12:11 +0100

    Commit automatico 30/01/2026 13:12:11,73

---
+++ TPSIT/Programmazione/Esercitazioni Scuola/Esercitazione 30-01-2026/javascript/script3.js
@@ -0,0 +1,15 @@
+let elencoPrezzi = []; // Array vuoto
+let i = 0; // Indice per l'inserimento degli alimenti inizializato a 0
+function salvaPrezzo() {
+    let nome = document.getElementById("numero").value * 1; // Prende il valore dall'input e lo converte in numero
+    document.getElementById("numero").value = "";
+    elencoPrezzi[i] = nome; // Aggiunge il prezzo all'array
+    i++; // Incrementa l'indice per il prossimo inserimento
+}
+
+function mostraPrezzo() {
+    document.getElementById("elencoPrezzi").innerHTML = ""; // Pulisce il contenuto precedente
+    for (let j = 0; j < elencoPrezzi.length; j++) { // Cicla attraverso l'array
+        document.getElementById("elencoPrezzi").innerHTML += elencoPrezzi[j] + "<br>"; // Aggiunge ogni prezzo all'elemento HTML
+    }
+}
