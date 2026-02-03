7760282cb5b03c04a4f7a39b8fc6111a296efcad
MatteoPorcu08 <porcumatteo@asproni-fermi.edu.it>
2026-02-03 15:32:22 +0100

    Commit automatico 03/02/2026 15:32:22,92

--- Informatica/Programmazione/Java/Classe CD/src/pacchetto/Main.java
+++ Informatica/Programmazione/Java/Classe CD/src/pacchetto/Main.java
@@ -5,9 +5,9 @@ public class Main {
 
         CD cd = new CD("Greatest Hits", "Sony Music", 2024);
 
-        Brano b1 = new Brano("Song One", "Artist A", 3.5f, 1);
-        Brano b2 = new Brano("Song Two", "Artist B", 4.2f, 2);
-        Brano b3 = new Brano("Song Three", "Artist C", 2.8f, 3);
+        Brano b1 = new Brano("Canzone 1", "Artist A", 3.5f, 1);
+        Brano b2 = new Brano("Canzone 2", "Artist B", 4.2f, 2);
+        Brano b3 = new Brano("Canzone 3", "Artist C", 2.8f, 3);
 
         cd.aggiungiBrano(b1);
         cd.aggiungiBrano(b2);
@@ -17,9 +17,9 @@ public class Main {
         System.out.println("Durata totale: " + cd.durataTotale());
 
         System.out.println("Ricerca per titolo:");
-        System.out.println(cd.cercaBrano("Song Two"));
+        System.out.println(cd.cercaBrano("Canzone 2"));
 
-        cd.rimuoviBrano("Song One");
+        cd.rimuoviBrano("Canzone 1");
 
         System.out.println("Brani sotto 4 minuti: " + cd.braniSotto4Minuti());
     }
