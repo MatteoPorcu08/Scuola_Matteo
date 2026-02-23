75984d6ef24e2464f39bf8f7c95862cde8787868
MatteoPorcu08 <porcumatteo@asproni-fermi.edu.it>
2026-02-23 09:35:39 +0100

    Commit automatico 23/02/2026  9:35:39,76

--- Informatica/Programmazione/Java/Gestione Libreria Videogiochi/src/gestione/Main.java
+++ Informatica/Programmazione/Java/Gestione Libreria Videogiochi/src/gestione/Main.java
@@ -1,5 +1,12 @@
 package gestione;
 
+/**
+ * Gestione di una libreria di videogiochi
+ * @author Matteo
+ * @version 3.1
+ * Data 23/02/2026s
+ */
+
 public class Main {
     public static void main(String[] args) {
 
