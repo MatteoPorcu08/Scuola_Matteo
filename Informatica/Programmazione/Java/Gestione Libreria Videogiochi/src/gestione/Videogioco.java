84dddb4263a7b8b3e215ecda045c081624c9882f
MatteoPorcu08 <porcumatteo@asproni-fermi.edu.it>
2026-02-23 08:50:16 +0100

    Commit automatico 23/02/2026  8:50:16,84

---
+++ Informatica/Programmazione/Java/Gestione Libreria Videogiochi/src/gestione/Videogioco.java
@@ -0,0 +1,22 @@
+package gestione;
+public class Videogioco {
+
+    private String titolo;
+    private String genere;
+    private int annoUscita;
+    private double prezzo;
+    private boolean multiplayer;
+
+    public Videogioco(String titolo, String genere, int annoUscita, double prezzo, boolean multiplayer) {
+        this.titolo = titolo;
+        this.genere = genere;
+        this.annoUscita = annoUscita;
+        this.prezzo = prezzo;
+        this.multiplayer = multiplayer;
+    }
+
+    public String getTitolo() {
+        return titolo;
+    }
+    pubbli
+}
