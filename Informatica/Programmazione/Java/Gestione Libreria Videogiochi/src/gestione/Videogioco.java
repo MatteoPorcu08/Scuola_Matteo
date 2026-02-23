3a117314121f32257db535943da37cf18b88af48
MatteoPorcu08 <porcumatteo@asproni-fermi.edu.it>
2026-02-23 08:55:19 +0100

    Commit automatico 23/02/2026  8:55:18,91

--- Informatica/Programmazione/Java/Gestione Libreria Videogiochi/src/gestione/Videogioco.java
+++ Informatica/Programmazione/Java/Gestione Libreria Videogiochi/src/gestione/Videogioco.java
@@ -7,6 +7,7 @@ public class Videogioco {
     private double prezzo;
     private boolean multiplayer;
 
+    //Costruttore
     public Videogioco(String titolo, String genere, int annoUscita, double prezzo, boolean multiplayer) {
         this.titolo = titolo;
         this.genere = genere;
@@ -15,8 +16,66 @@ public class Videogioco {
         this.multiplayer = multiplayer;
     }
 
+    //Costruttore di copia
+    public Videogioco(Videogioco altro) {
+        this.titolo = altro.titolo;
+        this.genere = altro.genere;
+        this.annoUscita = altro.annoUscita;
+        this.prezzo = altro.prezzo;
+        this.multiplayer = altro.multiplayer;
+    }
+
+    //Metodi getter
     public String getTitolo() {
         return titolo;
     }
-    pubbli
+    public String getGenere() {
+        return genere;
+    }
+    public int getAnnoUscita() {
+        return annoUscita;
+    }
+    public double getPrezzo() {
+        return prezzo;
+    }
+    public boolean isMultiplayer() {
+        return multiplayer;
+    }
+
+    //Metodo setter
+    public void setTitolo(String titolo) {
+        this.titolo = titolo;
+    }
+    public void setGenere(String genere) {
+        this.genere = genere;
+    }
+    public void setAnnoUscita(int annoUscita) {
+        this.annoUscita = annoUscita;
+    }
+    public void setPrezzo(double prezzo) {
+        this.prezzo = prezzo;
+    }
+    public void setMultiplayer(boolean multiplayer) {
+        this.multiplayer = multiplayer;
+    }
+
+    //Metodo toString
+    @Override
+    public String toString() {
+        return "Videogioco" +"titolo='" + titolo + '\'' +", genere='" + genere + '\'' +", annoUscita=" + annoUscita +", prezzo=" + prezzo +", multiplayer=" + multiplayer +'}';
+
+    }
+
+    //Metodo applicaSconto
+    public void applicaSconto(double percentuale) {
+        if (percentuale < 0 || percentuale > 100) {
+            System.out.println("Percentuale non valida. Deve essere compresa tra 0 e 100.");
+            return;
+        }
+        double sconto = prezzo * (percentuale / 100);
+        prezzo -= sconto;
+    }
+
+    //Metodo isRecente
+
 }
