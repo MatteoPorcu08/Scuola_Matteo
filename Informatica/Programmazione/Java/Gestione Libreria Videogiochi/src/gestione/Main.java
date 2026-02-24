b822e84bc70cd6037e1bd35c0e9089db58f849cb
MatteoPorcu08 <porcumatteo@asproni-fermi.edu.it>
2026-02-24 12:48:55 +0100

    Commit automatico 24/02/2026 12:48:55,07

--- Informatica/Programmazione/Java/Gestione Libreria Videogiochi/src/gestione/Main.java
+++ Informatica/Programmazione/Java/Gestione Libreria Videogiochi/src/gestione/Main.java
@@ -8,14 +8,15 @@ package gestione;
  */
 public class Main {
     public static void main(String[] args) {
-        
+
         System.out.println("=== TEST GESTIONE LIBRERIA VIDEOGIOCHI ===\n");
-        
-        // 1. Creare una nuova Libreria
-        Libreria libreria = new Libreria();
+
+
+        System.out.println("1. Creazione nuova libreria:");
+        Libreria libreria = new Libreria(10);
         System.out.println("1. Libreria creata con successo (dimensione 10)");
-        
-        // 2. Creare almeno 6 oggetti Videogioco e inserirli
+
+
         System.out.println("\n2. Creazione e inserimento di 6 videogiochi:");
         
         Videogioco gioco1 = new Videogioco("The Last of Us", "Azione", 2013, 59.99, false);
@@ -24,7 +25,7 @@ public class Main {
         Videogioco gioco4 = new Videogioco("God of War", "Azione", 2022, 49.99, false);
         Videogioco gioco5 = new Videogioco("Mario Kart", "Corse", 2020, 39.99, true);
         Videogioco gioco6 = new Videogioco("Cyberpunk 2077", "RPG", 2020, 34.99, false);
-        
+
         libreria.aggiungiVideogioco(gioco1);
         libreria.aggiungiVideogioco(gioco2);
         libreria.aggiungiVideogioco(gioco3);
@@ -32,68 +33,6 @@ public class Main {
         libreria.aggiungiVideogioco(gioco5);
         libreria.aggiungiVideogioco(gioco6);
         
-        System.out.println("Inseriti 6 videogiochi");
         
-        // 3. Stampare la libreria
-        System.out.println("\n3. Visualizzazione libreria iniziale:");
-        libreria.visualizzaLibreria();
-        
-        // 4. Rimuovere il gioco in posizione 2 e stampare
-        System.out.println("4. Rimozione gioco in posizione 2:");
-        boolean rimossa = libreria.rimuoviVideogioco(2);
-        System.out.println("Rimozione " + (rimossa ? "riuscita" : "fallita"));
-        libreria.visualizzaLibreria();
-        
-        // 5. Rimuovere un videogioco per titolo
-        System.out.println("5. Rimozione gioco con titolo 'FIFA 24':");
-        boolean rimossaTitolo = libreria.rimuoviTitolo("FIFA 24");
-        System.out.println("Rimozione " + (rimossaTitolo ? "riuscita" : "fallita"));
-        libreria.visualizzaLibreria();
-        
-        // 6. Applicare sconto del 20% a tutti
-        System.out.println("6. Applicazione sconto del 20% a tutti i giochi:");
-        libreria.applicaScontoATutti(20);
-        libreria.visualizzaLibreria();
-        
-        // 7. Stampare il videogioco più costoso
-        System.out.println("7. Videogioco più costoso:");
-        Videogioco piuCostoso = libreria.trovaPiuCostoso();
-        if (piuCostoso != null) {
-            System.out.println(piuCostoso);
-        } else {
-            System.out.println("Nessun gioco presente");
-        }
-        
-        // 8. Ordinare per prezzo crescente e stampare
-        System.out.println("\n8. Ordinamento per prezzo crescente:");
-        libreria.ordinaPerPrezzoCrescente();
-        libreria.visualizzaLibreria();
-        
-        // Test aggiuntivi
-        System.out.println("\n=== TEST AGGIUNTIVI ===");
-        
-        // Test contaMultiplayer
-        System.out.println("Giochi multiplayer: " + libreria.contaMultiplayer());
-        
-        // Test calcolaValoreTotale e mediaPrezzi
-        System.out.println("Valore totale libreria: €" + libreria.calcolaValoreTotale());
-        System.out.println("Media prezzi: €" + libreria.mediaPrezzi());
-        
-        // Test filtraPerGenere
-        System.out.println("\nFiltro per genere 'Azione':");
-        Videogioco[] azione = libreria.filtraPerGenere("Azione");
-        for (Videogioco g : azione) {
-            System.out.println(g);
-        }
-        
-        // Test ordinamento per anno
-        System.out.println("\nOrdinamento per anno (decrescente):");
-        libreria.ordinaPerAnnoDecrescente();
-        libreria.visualizzaLibreria();
-        
-        // Test compattazione
-        System.out.println("Compattazione libreria:");
-        libreria.compattaLibreria();
-        libreria.visualizzaLibreria();
     }
 }
