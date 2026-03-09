f240f9318fe5871c3d4b6e39d2a106e01b5c79e2
MatteoPorcu08 <porcumatteo@asproni-fermi.edu.it>
2026-03-09 08:55:02 +0100

    Commit automatico 09/03/2026  8:55:02,75

--- Informatica/Programmazione/Java/Gestione Centro Sportivo/src/PrenotazioneCorsi/Corso.java
+++ Informatica/Programmazione/Java/Gestione Centro Sportivo/src/PrenotazioneCorsi/Corso.java
@@ -19,4 +19,67 @@ public class Corso {
     }
 
     //Costruttore di copia
+    public Corso(Corso corso) {
+        this.nome = corso.nome;
+        this.istruttore= corso.istruttore;
+        this.durata= corso.durata;
+        this.costoMensile= corso.costoMensile;
+        this.numeroIscritti = corso.numeroIscritti;
+        this.capMaxIscritti = corso.capMaxIscritti;
+    }
+
+    //Getter
+    public String getNome() {
+        return nome;
+    }
+
+    public String getIstruttore() {
+        return istruttore;
+    }
+
+    public int getDurata() {
+        return durata;
+    }
+
+    public double getCostoMensile() {
+        return costoMensile;
+    }
+
+    public int getNumeroIscritti() {
+        return numeroIscritti;
+    }
+
+    public int getCapMaxIscritti() {
+        return capMaxIscritti;
+    }
+
+    //Setter
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    public void setIstruttore(String istruttore) {
+        this.istruttore = istruttore;
+    }
+    public void setDurata(int durata) {
+        this.durata = durata;
+    }
+    public void setCostoMensile(double costoMensile) {
+        this.costoMensile = costoMensile;
+    }
+    public void setNumeroIscritti(int numeroIscritti) {
+        this.numeroIscritti = numeroIscritti;
+    }
+    public void setCapMaxIscritti(int capMaxIscritti) {
+        this.capMaxIscritti = capMaxIscritti;
+    }
+
+    //Metodo ToString
+    @Override
+    public String toString() {
+        return "Corso:"+ nome+ "\nIstruttore: "+ istruttore+
+                "\nDurata: "+ durata+ " minuti"+
+                "\nCosto Mensile: "+ costoMensile+ " euro"+
+                "\nNumero Iscritti: "+ numeroIscritti+
+                "\nCapacità Massima Iscritti: "+ capMaxIscritti;
+    }
 }
