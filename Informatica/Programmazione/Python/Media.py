c16e434e71601d078888c2032762a645de4c21ef
MatteoPorcu08 <porcumatteo@asproni-fermi.edu.it>
2026-03-10 16:53:40 +0100

    Commit automatico 10/03/2026 16:53:40,42

---
+++ Informatica/Programmazione/Python/Media.py
@@ -0,0 +1,12 @@
+A=0
+B=0
+C=0
+
+print ("Inserisci il primo numero: ")
+A=int (input())
+print ("Inserisci il secondo numero: ")
+B=int (input())
+print ("Inserisci il terzo numero: ")
+C=int (input())
+
+print("La media dei tre numeri e': ", (A+B+C)/3)
