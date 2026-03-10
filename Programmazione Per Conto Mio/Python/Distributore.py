f0c292c545018a6d2a32c87e7655b2cb0a28ccad
MatteoPorcu08 <porcumatteo@asproni-fermi.edu.it>
2026-03-10 16:38:33 +0100

    Commit automatico 10/03/2026 16:38:32,95

--- Programmazione Per Conto Mio/Python/Distributore.py
+++ Programmazione Per Conto Mio/Python/Distributore.py
@@ -1,1 +1,7 @@
-int M
+M = 0
+print("Inserisci un euro: ")
+M = int(input())
+if M >= 1:
+    print("Ecco a te la bevanda")
+else:
+    print("Errore, devi inserire almeno un euro")
---
+++ Programmazione Per Conto Mio/Python/Velocita.py
@@ -0,0 +1,3 @@
+v=0
+print ("A che velocita stai andando? ")
+v=int (input())
