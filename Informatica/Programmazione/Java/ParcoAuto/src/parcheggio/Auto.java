42ad9192ae0f177c973443116c403ccd9c13c05d
MatteoPorcu08 <porcumatteo@asproni-fermi.edu.it>
2026-03-17 12:54:36 +0100

    Commit automatico 17/03/2026 12:54:36,06

--- Informatica/Programmazione/Java/ParcoAuto/src/parcheggio/Auto.java
+++ Informatica/Programmazione/Java/ParcoAuto/src/parcheggio/Auto.java
@@ -31,6 +31,47 @@ public class Auto {
     public String getTarga() {
         return targa;
     }
-    public 
+    public String getModello() {
+        return modello;
+    }
+    public int getAnno() {
+        return anno;
+    }
+    public int getKm() {
+        return km;
+    }
+    public boolean isDisponibile() {
+        return disponibile;
+    }
+    public double getPrezzoGiornaliero() {
+        return prezzoGiornaliero;
+    }
 
+    //Setter
+    public void setTarga(String targa) {
+        this.targa = targa;
+    }
+    public void setModello(String modello) {
+        this.modello = modello;
+    }
+    public void setAnno(int anno) {
+        this.anno = anno;
+    }
+    public void setKm(int km) {
+        this.km = km;
+    }
+    public void setDisponibile(boolean disponibile) {
+        this.disponibile = disponibile;
+    }
+    public void setPrezzoGiornaliero(double prezzoGiornaliero) {
+        this.prezzoGiornaliero = prezzoGiornaliero;
+    }
+
+    //Metodo toString
+    @Override
+    public String toString() {
+        return "Auto:\n Targa: " + targa + "\n Modello: " + modello + "\n Anno: " + anno + "\n Km: " + km + "\n Disponibile: " + disponibile + "\n Prezzo Giornaliero: " + prezzoGiornaliero;
+    }
+
+    //Metodo pubblic void restituisci(int kmaggiunti)
 }
