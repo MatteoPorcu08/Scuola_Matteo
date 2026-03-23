6698685d1607b1ded9d9d7901335291a9282a9ac
MatteoPorcu08 <porcumatteo@asproni-fermi.edu.it>
2026-03-23 08:50:31 +0100

    Commit automatico 23/03/2026  8:50:31,92

--- Informatica/Programmazione/Java/ParcoAuto/src/parcheggio/ParcoAuto.java
+++ Informatica/Programmazione/Java/ParcoAuto/src/parcheggio/ParcoAuto.java
@@ -1,23 +1,35 @@
 package parcheggio;
 
 public class ParcoAuto {
-    private static final int NUM_MAX= 20;
+    private static final int NUM_MAX = 20;
     private Auto[] auto;
     private int numeroAuto;
 
-    //Costruttore
+    // Costruttore
     public ParcoAuto() {
         auto = new Auto[NUM_MAX];
         this.numeroAuto = 0;
     }
 
-    //Costruttore di copia
+    // Costruttore di copia
     public ParcoAuto(ParcoAuto p) {
         auto = new Auto[NUM_MAX];
         numeroAuto = p.numeroAuto;
         for (int i = 0; i < numeroAuto; i++) {
             auto[i] = new Auto(p.auto[i]);
         }
+    }
+
+    // Metodo public boolean rimuoviAuto(int posizione)
+    public boolean rimuoviAuto(int posizione) {
+        if (posizione < 0 || posizione >= numeroAuto) {
+            return false;
+        }
+        for (int i = posizione; i < numeroAuto - 1; i++) {
+            auto[i] = auto[i + 1];
+        }
+        numeroAuto--;
+        return true;
     }
 
     
