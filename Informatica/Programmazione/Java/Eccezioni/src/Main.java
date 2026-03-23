136aa856a47a2f015d335f7f3136986dc6105ed2
MatteoPorcu08 <porcumatteo@asproni-fermi.edu.it>
2026-03-23 09:41:00 +0100

    Commit automatico 23/03/2026  9:41:00,83

---
+++ Informatica/Programmazione/Java/Eccezioni/src/Main.java
@@ -0,0 +1,21 @@
+public class Main {
+    public static void main(String[] args) {
+        // Divisione per zero
+        int a = 5;
+        int b = 0;
+        try {
+            int c = a / b;
+            System.out.println(c);
+        } catch (ArithmeticException e) {
+            System.out.println("Errore: divisione per zero");
+        }
+
+        // Array Index Out Of Bounds
+        int[] arr = { 1, 2, 3 };
+        try {
+            System.out.println(arr[5]);
+        } catch (ArrayIndexOutOfBoundsException e) {
+            System.out.println("Errore: indice fuori dai limiti dell'array");
+        }
+    }
+}
