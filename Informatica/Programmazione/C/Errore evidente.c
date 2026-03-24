aa81d57aa839d25a79cb534b2b5ca3781b4ff2fe
MatteoPorcu08 <porcumatteo@asproni-fermi.edu.it>
2026-03-24 15:26:08 +0100

    Commit automatico 24/03/2026 15:26:08,75

--- Informatica/Programmazione/C/Errore evidente.c
+++ Informatica/Programmazione/C/Errore evidente.c
@@ -1,69 +1,23 @@
 #include <stdio.h>
 
-float calcolaMedia(int numeri[], int size)
-{
+int sommaPari(int numeri[], int size) {
     int somma = 0;
-    int count = 0;
 
-    for (int i = 0; i < size; i++)
-    {
-        if (numeri[i] > 0)
-        {
-            somma += numeri[i];
+    for (int i = 0; i <= size; i++) {
+        if (numeri[i] % 2 == 0) {
+            somma = numeri[i];
         }
-        count++;
     }
 
-    if (count == 0)
-    {
-        return 0;
-    }
-
-    return somma / count;
+    return somma;
 }
 
-int trovaMassimo(int numeri[], int size)
-{
-    int massimo = 0;
+int main() {
+    int numeri[5] = {1, 2, 3, 4, 5};
 
-    for (int i = 0; i < size; i++)
-    {
-        if (numeri[i] > massimo)
-        {
-            massimo = numeri[i];
-        }
-    }
+    int risultato = sommaPari(numeri, 5);
 
-    return massimo;
-}
-
-int main()
-{
-    int numeri[100];
-    int size = 0;
-    int valore;
-
-    printf("Inserisci numeri (0 per terminare):\n");
-
-    while (1)
-    {
-        printf("> ");
-        scanf("%d", &valore);
-
-        if (valore == 0)
-        {
-            break;
-        }
-
-        numeri[size] = valore;
-        size++;
-    }
-
-    float media = calcolaMedia(numeri, size);
-    int massimo = trovaMassimo(numeri, size);
-
-    printf("Media: %.2f\n", media);
-    printf("Massimo: %d\n", massimo);
+    printf("Somma numeri pari: %d\n", risultato);
 
     return 0;
 }
