391273da26bfc291ccb10cf5b216dd209b315626
MatteoPorcu08 <porcumatteo@asproni-fermi.edu.it>
2026-03-27 13:02:20 +0100

    Commit automatico 27/03/2026 13:02:20,73

---
+++ TPSIT/Programmazione/Esercitazioni Scuola/Esercitazione5 Array 27-03-2026/javascript/script.js
@@ -0,0 +1,66 @@
+function aggiungiPrenotazione() {
+    let nomeCliente = document.getElementById("nomeCliente").value;
+    let nomeCorso = document.getElementById("nomeCorso").value;
+    
+    if (nomeCliente === "" || nomeCorso === "") {
+        alert("Errore: Il nome del cliente e del corso non possono essere vuoti.");
+        return;
+    }
+    if (clienti.includes(nomeCliente)) {
+        alert("Cliente già prenotato.");
+        return;
+    }
+    
+    clienti.push(nomeCliente);
+    corsi.push(nomeCorso);
+    
+    alert("Prenotazione aggiunta correttamente.");
+    
+    document.getElementById("nomeCliente").value = "";
+    document.getElementById("nomeCorso").value = "";
+}
+
+function ricercaPrenotazione() {
+    let nomeCliente = prompt("Inserisci il nome del cliente da cercare:");
+    
+    let index = clienti.indexOf(nomeCliente);
+    
+    if (index !== -1) {
+        alert("Il cliente " + nomeCliente + " ha prenotato il corso: " + corsi[index]);
+    } else {
+        alert("Cliente non presente.");
+    }
+}
+
+function eliminaPrenotazione() {
+    let nomeCliente = prompt("Inserisci il nome del cliente da eliminare:");
+    
+    let index = clienti.indexOf(nomeCliente);
+    
+    if (index !== -1) {
+        let conferma = prompt("Sei sicuro di voler eliminare la prenotazione di " + nomeCliente + "? (S/N)");
+        
+        if (conferma === "S") {
+            clienti.splice(index, 1);
+            corsi.splice(index, 1);
+            alert("Prenotazione eliminata.");
+        }
+    } else {
+        alert("Cliente non trovato.");
+    }
+}
+
+function visualizzaPrenotazioni() {
+    let prenotazioni = document.getElementById("prenotazioni");
+    
+    if (clienti.length === 0) {
+        prenotazioni.innerHTML = "Nessuna prenotazione presente.";
+    } else {
+        prenotazioni.innerHTML = "";
+        for (let i = 0; i < clienti.length; i++) {
+            prenotazioni.innerHTML += clienti[i] + " - " + corsi[i] + "<br>";
+        }
+    }
+}
+let clienti = [];
+let corsi = [];
