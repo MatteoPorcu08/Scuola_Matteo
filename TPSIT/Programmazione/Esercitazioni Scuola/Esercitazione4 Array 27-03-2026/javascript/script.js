ee071c3c481bf2499acc3b061e6fc72d912f9a66
MatteoPorcu08 <porcumatteo@asproni-fermi.edu.it>
2026-03-27 12:16:38 +0100

    Commit automatico 27/03/2026 12:16:38,77

---
+++ TPSIT/Programmazione/Esercitazioni Scuola/Esercitazione4 Array 27-03-2026/javascript/script.js
@@ -0,0 +1,91 @@
+// Array
+let studenti = [];
+let voti = [];
+let i = 0;
+
+// Inserisci studente
+function inserisciStudente() {
+    let nome = prompt("Inserisci nome studente:");
+    let voto = prompt("Inserisci voto:") * 1;
+
+    if (nome && voto == voto) {
+        studenti[i] = nome;
+        voti[i] = voto;
+        i++;
+        alert("Studente inserito!");
+    } else {
+        alert("Dati non validi");
+    }
+}
+
+// Visualizza registro (in pagina)
+function visualizzaRegistro() {
+    let output = "";
+
+    for (let j = 0; j < studenti.length; j++) {
+        output += studenti[j] + " : " + voti[j] + "<br>";
+    }
+
+    document.getElementById("registro").innerHTML = output;
+}
+
+// Calcola media (alert)
+function calcolaMedia() {
+    if (voti.length === 0) {
+        alert("Nessun voto");
+        return;
+    }
+
+    let somma = 0;
+
+    for (let j = 0; j < voti.length; j++) {
+        somma += voti[j];
+    }
+
+    let media = somma / voti.length;
+
+    alert("Media: " + media.toFixed(2));
+}
+
+// Voto più alto
+function trovaVotoPiuAlto() {
+    if (voti.length === 0) {
+        alert("Nessun voto");
+        return;
+    }
+
+    let max = voti[0];
+    let pos = 0;
+
+    for (let j = 1; j < voti.length; j++) {
+        if (voti[j] > max) {
+            max = voti[j];
+            pos = j;
+        }
+    }
+
+    alert("Voto più alto: " + max + " - " + studenti[pos]);
+}
+
+// Insufficienze (in pagina)
+function trovaInsufficienze() {
+    let output = "";
+
+    for (let j = 0; j < voti.length; j++) {
+        if (voti[j] < 6) {
+            output += studenti[j] + " : " + voti[j] + "<br>";
+        }
+    }
+
+    if (output === "") {
+        alert("Nessuna insufficienza");
+    } else {
+        document.getElementById("insufficienze").innerHTML = output;
+    }
+}
+
+// Cerca studente al click verrà richiesto tramite prompt il nome dello studente e, se presente, visualizzato in un alert il voto ottenuto; se non presente verrà visualizzato (sempre in un alert) "Alunno non presente".
+function cercaStudente() {
+    let nome = prompt("Inserisci nome studente da cercare:");
+    let trovato = false;
+    
