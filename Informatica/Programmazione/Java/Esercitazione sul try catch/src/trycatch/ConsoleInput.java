f85c7c9ae5a5e948b6b2f6003d3657598aaac703
MatteoPorcu08 <porcumatteo@asproni-fermi.edu.it>
2026-03-30 09:04:39 +0200

    Commit automatico 30/03/2026  9:04:39,00

---
+++ Informatica/Programmazione/Java/Esercitazione sul try catch/src/trycatch/ConsoleInput.java
@@ -0,0 +1,41 @@
+package trycatch;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class ConsoleInput {
+    /**
+     * @author 4B INF
+     * @version 1.0
+     *          Classe che useremo per gli input con la classe BufferedReader
+     */
+    BufferedReader input;
+
+    /**
+     * Costruttore
+     */
+    public ConsoleInput() {
+        input = new BufferedReader(new InputStreamReader(System.in));
+    }
+
+    /**
+     * Metodo che serve per acquisire i numeri interi
+     * 
+     * @return numero intero acquisito con l'input
+     */
+
+    public int readInt() throws IOException {
+        return Integer.parseInt(input.readLine());
+    }
+
+    /**
+     * Metodo che serve per acquisire i numeri double
+     * 
+     * @return numero double acquisito con l'input
+     */
+    public double readDouble() throws IOException {
+        return Double.parseDouble(input.readLine());
+    }
+
+}
--- Informatica/Programmazione/Java/Esercitazione sul try catch/src/trycatch/Main.java
+++ Informatica/Programmazione/Java/Esercitazione sul try catch/src/trycatch/Main.java
@@ -4,8 +4,17 @@ import java.io.*;
 
 public class Main {
     public static void main(String[] args) {
-        BufferedReader input;
+        ConsoleInput console = new ConsoleInput();
         // Esercizio 5
+        try {
+            System.out.print("Inserisci l'età: ");
+            int num = console.readInt();
+            System.out.println("Hai inserito: " + num);
+        } catch (IOException e) {
+            System.out.println("Errore di input/output: " + e.getMessage());
+        } catch (NumberFormatException e) {
+            System.out.println("Errore di formato: non hai inserito un numero intero valido.");
+        }
 
         // Esercizio 6
     }
