dbc344c35e1ab913c1122c65fb866a627200ef1e
MatteoPorcu08 <porcumatteo@asproni-fermi.edu.it>
2026-03-30 09:29:48 +0200

    Commit automatico 30/03/2026  9:29:48,83

--- Informatica/Programmazione/Java/Esercitazione sul try catch/src/trycatch/Main.java
+++ Informatica/Programmazione/Java/Esercitazione sul try catch/src/trycatch/Main.java
@@ -12,9 +12,23 @@ public class Main {
             System.out.println("Hai inserito: " + num);
         } catch (NumberFormatException e) {
             System.out.println("Errore di formato: non hai inserito un numero intero valido.");
+        } catch (IOException e) {
+            System.out.println("Errore di input/output: " + e.getMessage());
         }
 
         // Esercizio 6
-
+        double somma = 0;
+        for (int i = 0; i < 3; i++) {
+            try {
+                System.out.print("Inserisci un numero double: ");
+                double num = console.readDouble();
+                somma += num;
+            } catch (NumberFormatException e) {
+                System.out.println("Errore di formato: non hai inserito un numero double valido.");
+            } catch (IOException e) {
+                System.out.println("Errore di input/output: " + e.getMessage());
+            }
+        }
+        System.out.println("La somma dei numeri inseriti è: " + somma);
     }
 }
