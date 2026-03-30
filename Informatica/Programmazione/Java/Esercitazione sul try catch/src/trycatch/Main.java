9d22a84ba293fc028fcc23429a3e6df49801e7a1
MatteoPorcu08 <porcumatteo@asproni-fermi.edu.it>
2026-03-30 08:59:36 +0200

    Commit automatico 30/03/2026  8:59:36,86

--- Informatica/Programmazione/Java/Esercitazione sul try catch/src/trycatch/Main.java
+++ Informatica/Programmazione/Java/Esercitazione sul try catch/src/trycatch/Main.java
@@ -1,18 +1,12 @@
 package trycatch;
 
+import java.io.*;
+
 public class Main {
     public static void main(String[] args) {
-        try {
-            System.out.println("Inserisci la tua età:");
-            int age = Integer.parseInt(System.console().readLine());
-            if (age < 0) {
-                throw new IllegalArgumentException("L'età non può essere negativa.");
-            }
-            System.out.println("La tua età è: " + age);
-        } catch (NumberFormatException e) {
-            System.out.println("Errore: input non numerico. Per favore, inserisci un numero valido.");
-        } catch (IllegalArgumentException e) {
-            System.out.println("Errore: " + e.getMessage());
-        }
+        BufferedReader input;
+        // Esercizio 5
+
+        // Esercizio 6
     }
 }
