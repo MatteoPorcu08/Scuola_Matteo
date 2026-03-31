18d825dab76822c31595208732f2fad5690ba665
MatteoPorcu08 <porcumatteo@asproni-fermi.edu.it>
2026-03-31 13:06:15 +0200

    Commit automatico 31/03/2026 13:06:15,80

--- Informatica/Programmazione/Java/Saldo bancario/src/soldi/Main.java
+++ Informatica/Programmazione/Java/Saldo bancario/src/soldi/Main.java
@@ -2,8 +2,17 @@ package soldi;
 
 public class Main {
     public static void main(String[] args) {
-        ContoCorrente conto = new ContoCorrente(100);
+        ContoCorrente conto = new ContoCorrente(100); // Saldo iniziale di 100
 
-        ContoCorrente.
+        try {
+            conto.verificaSaldo(150); // Verifica se è possibile effettuare un'operazione di 150
+            conto.effettuaOperazione(150); // Effettua l'operazione di 150
+        } catch (SaldoInsufficienteException e) {
+            System.out.println(e.toString()); // Stampa il messaggio di eccezione se il saldo è insufficiente
+        }
+
+        finally {
+            System.out.println("Operazione completata.");
+        }
     }
 }
