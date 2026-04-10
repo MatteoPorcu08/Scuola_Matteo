49c8eabbbd7e784a3c0587dddcb96d3302e35421
MatteoPorcu08 <porcumatteo@asproni-fermi.edu.it>
2026-04-10 12:23:12 +0200

    Commit automatico 10/04/2026 12:23:12,90

--- TPSIT/Programmazione/Esercitazioni Scuola/Esercitazione6 Array 10-04-2026/JavaScript/script.js
+++ TPSIT/Programmazione/Esercitazioni Scuola/Esercitazione6 Array 10-04-2026/JavaScript/script.js
@@ -7,11 +7,11 @@ let i = 0;
 function registraPrestito() {
     let libro = document.getElementById("libro");
     let nome = document.getElementById("utente");
-    let giorni = document.getElementById("giorni")*1;
+    let giorni = document.getElementById("giorni") * 1;
     let risultatoControllo = ricercaElemento(libro);
 
     if (risultatoControllo != -1) {
-        alert("Il libro è già stato prestato a " + nomeUtente[risultatoControllo] + " per " + giorniPrestito[risultatoControllo] + " giorni.");
+        alert("Il libro è già stato prestato");
     } else {
     titoloLibro[i] = libro;
     nomeUtente[i] = nome;
