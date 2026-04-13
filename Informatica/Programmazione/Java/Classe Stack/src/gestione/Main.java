d4f0ac5220ca3866a9c936abc1ea2db8c1133600
MatteoPorcu08 <porcumatteo@asproni-fermi.edu.it>
2026-04-13 09:09:16 +0200

    Commit automatico 13/04/2026  9:09:16,73

--- Informatica/Programmazione/Java/Classe Stack/src/gestione/Main.java
+++ Informatica/Programmazione/Java/Classe Stack/src/gestione/Main.java
@@ -1,24 +1,28 @@
 package gestione;
+import java.util.Stack;
 
 public class Main {
     public static void main(String[] args) {
         Stack stack = new Stack(2);
 
-        stack.push(1);
-        stack.push(2);
-        stack.push(3); // Stack pieno, non viene aggiunto
-
         try {
-            
+            stack.push(1);
+            stack.push(2);
+            stack.push(3);
+
         } catch (Exception e) {
             System.out.println("Errore: " + e.getMessage());
+
+        } finally {
+            System.out.println("Operazione terminata");
         }
 
         try {
-            stack.push(4);
-            System.out.println(stack.pop()); // Stampa 4
+            System.out.println("Elemento estratto: " + stack.pop());
+            System.out.println("Elemento estratto: " + stack.pop());
+            System.out.println("Elemento estratto: " + stack.pop());
         } catch (Exception e) {
-            System.out.println("Errore: " + e.getMessage());
+            System.out.println("Errore durante pop: " + e.getMessage());
         }
     }
 }
