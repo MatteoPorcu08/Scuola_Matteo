6610472eb08938e8f5dd76051b87271b3676ccc4
MatteoPorcu08 <porcumatteo@asproni-fermi.edu.it>
2026-04-13 09:04:14 +0200

    Commit automatico 13/04/2026  9:04:14,81

--- Informatica/Programmazione/Java/Classe Stack/src/gestione/Main.java
+++ Informatica/Programmazione/Java/Classe Stack/src/gestione/Main.java
@@ -7,5 +7,18 @@ public class Main {
         stack.push(1);
         stack.push(2);
         stack.push(3); // Stack pieno, non viene aggiunto
+
+        try {
+            
+        } catch (Exception e) {
+            System.out.println("Errore: " + e.getMessage());
+        }
+
+        try {
+            stack.push(4);
+            System.out.println(stack.pop()); // Stampa 4
+        } catch (Exception e) {
+            System.out.println("Errore: " + e.getMessage());
+        }
     }
 }
