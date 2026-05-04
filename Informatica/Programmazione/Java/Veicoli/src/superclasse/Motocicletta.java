fcfaa6a122de2834c1e4b11b4bb0c2e90f7c8328
MatteoPorcu08 <porcumatteo@asproni-fermi.edu.it>
2026-05-04 09:44:57 +0200

    Commit automatico 04/05/2026  9:44:57,86

--- Informatica/Programmazione/Java/Veicoli/src/superclasse/Motocicletta.java
+++ Informatica/Programmazione/Java/Veicoli/src/superclasse/Motocicletta.java
@@ -1,66 +1,49 @@
 package superclasse;
-public class Motocicletta {
-    public String marca;
-    public  String modello;
-    public  int noleggioGiornaliero;
-    public int cilindrata;
-    public boolean bauletto;
+public class Motocicletta extends Veicolo {
+    private int cilindrata;
+    private boolean bauletto;
 
-    public Motocicletta(String marca, String modello, int noleggioGiornaliero, int cilindrata, boolean bauletto) {
-        this.marca = marca;
-        this.modello = modello;
-        this.noleggioGiornaliero = noleggioGiornaliero;
+    public Motocicletta(String marca, String modello, int anno, double prezzoGiornaliero, int cilindrata, boolean bauletto) {
+        super(marca, modello, anno, prezzoGiornaliero);
         this.cilindrata = cilindrata;
         this.bauletto = bauletto;
     }
 
     // Getters
-    public String getMarca() {
-        return marca;
-    }
-    public String getModello() {
-        return modello;
-    }
-    public int getNoleggioGiornaliero() {
-        return noleggioGiornaliero;
-    }
     public int getCilindrata() {
         return cilindrata;
     }
+
     public boolean isBauletto() {
         return bauletto;
     }
 
     // Setters
-    public void setMarca(String marca) {
-        this.marca = marca;
-    }
-    public void setModello(String modello) {
-        this.modello = modello;
-    }
-    public void setNoleggioGiornaliero(int noleggioGiornaliero) {
-        this.noleggioGiornaliero = noleggioGiornaliero;
-    }
     public void setCilindrata(int cilindrata) {
         this.cilindrata = cilindrata;
     }
+
     public void setBauletto(boolean bauletto) {
         this.bauletto = bauletto;
     }
 
+    // Override del metodo stampaDettagli()
     @Override
-    public String toString() {
-        return "Motocicletta{" +
-                "marca='" + marca + '\'' +
-                ", modello='" + modello + '\'' +
-                ", noleggioGiornaliero=" + noleggioGiornaliero +
-                ", cilindrata=" + cilindrata +
-                ", bauletto=" + bauletto +
-                '}';
+    public void stampaDettagli() {
+        super.stampaDettagli();
+        System.out.println("Cilindrata: " + cilindrata);
+        System.out.println("Bauletto: " + bauletto);
     }
 
-    //Metodo calcolaCosto(int giorni)
+    // Override del metodo calcolaCosto(int giorni)
+    @Override
     public int calcolaCosto(int giorni) {
-        return noleggioGiornaliero * giorni;
+        int costoBase = super.calcolaCosto(giorni);
+        if (prezzoGiornaliero < 0) {
+            return 0;
+        }
+        else if( giorni )
+        return costoBase;
     }
+    
 }
