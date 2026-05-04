1c9b20638343e03c2b9431a3f25eb1e947693869
MatteoPorcu08 <porcumatteo@asproni-fermi.edu.it>
2026-05-04 09:24:46 +0200

    Commit automatico 04/05/2026  9:24:46,85

--- Informatica/Programmazione/Java/Veicoli/src/superclasse/Automobile.java
+++ Informatica/Programmazione/Java/Veicoli/src/superclasse/Automobile.java
@@ -1,15 +1,29 @@
 package superclasse;
-
 public class Automobile {
+    public String marca;
+    public  String modello;
+    public  int noleggioGiornaliero;
     private int numeroPorte;
     private String tipoAlimentazione;
 
-    public Automobile(int numeroPorte, String tipoAlimentazione) {
+    public Automobile(String marca, String modello, int noleggioGiornaliero, int numeroPorte, String tipoAlimentazione) {
+        this.marca = marca;
+        this.modello = modello;
+        this.noleggioGiornaliero = noleggioGiornaliero;
         this.numeroPorte = numeroPorte;
         this.tipoAlimentazione = tipoAlimentazione;
     }
 
     // Getters
+    public String getMarca() {
+        return marca;
+    }
+    public String getModello() {
+        return modello;
+    }
+    public int getNoleggioGiornaliero() {
+        return noleggioGiornaliero;
+    }
     public int getNumeroPorte() {
         return numeroPorte;
     }
@@ -18,6 +32,15 @@ public class Automobile {
     }
 
     //Setters
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+    public void setModello(String modello) {
+        this.modello = modello;
+    }
+    public void setNoleggioGiornaliero(int noleggioGiornaliero) {
+        this.noleggioGiornaliero = noleggioGiornaliero;
+    }
     public void setNumeroPorte(int numeroPorte) {
         this.numeroPorte = numeroPorte;
     }
@@ -28,9 +51,16 @@ public class Automobile {
     @Override
     public String toString() {
         return "Automobile{" +
-                "numeroPorte=" + numeroPorte +
+                "marca='" + marca + '\'' +
+                ", modello='" + modello + '\'' +
+                ", noleggioGiornaliero=" + noleggioGiornaliero +
+                ", numeroPorte=" + numeroPorte +
                 ", tipoAlimentazione='" + tipoAlimentazione + '\'' +
                 '}';
     }
 
+    //Metodo calcolaCosto(int giorni)
+    public int calcolaCosto(int giorni) {
+        return giorni * 50; // Costo fisso di 50 euro al giorno
+    }
 }
---
+++ Informatica/Programmazione/Java/Veicoli/src/superclasse/Motocicletta.java
@@ -0,0 +1,50 @@
+package superclasse;
+public class Motocicletta {
+    public String marca;
+    public  String modello;
+    public  int noleggioGiornaliero;
+    public int cilindrata;
+    public boolean bauletto;
+
+    public Motocicletta(String marca, String modello, int noleggioGiornaliero, int cilindrata, boolean bauletto) {
+        this.marca = marca;
+        this.modello = modello;
+        this.noleggioGiornaliero = noleggioGiornaliero;
+        this.cilindrata = cilindrata;
+        this.bauletto = bauletto;
+    }
+
+    // Getters
+    public String getMarca() {
+        return marca;
+    }
+    public String getModello() {
+        return modello;
+    }
+    public int getNoleggioGiornaliero() {
+        return noleggioGiornaliero;
+    }
+    public int getCilindrata() {
+        return cilindrata;
+    }
+    public boolean isBauletto() {
+        return bauletto;
+    }
+
+    // Setters
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+    public void setModello(String modello) {
+        this.modello = modello;
+    }
+    public void setNoleggioGiornaliero(int noleggioGiornaliero) {
+        this.noleggioGiornaliero = noleggioGiornaliero;
+    }
+    public void setCilindrata(int cilindrata) {
+        this.cilindrata = cilindrata;
+    }
+    public void setBauletto(boolean bauletto) {
+        this.bauletto = bauletto;
+    }
+}
