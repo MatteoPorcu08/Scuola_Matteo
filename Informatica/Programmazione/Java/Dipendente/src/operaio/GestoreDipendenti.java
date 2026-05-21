6d4ebccdb03d1f0f379deffef5540e53bd3a148e
MatteoPorcu08 <porcumatteo@asproni-fermi.edu.it>
2026-05-21 10:03:26 +0200

    Commit automatico 21/05/2026 10:03:26,78

--- Informatica/Programmazione/Java/Dipendente/src/operaio/GestoreDipendenti.java
+++ Informatica/Programmazione/Java/Dipendente/src/operaio/GestoreDipendenti.java
@@ -1,6 +1,36 @@
 package operaio;
-
 public class GestoreDipendenti {
     private Dipendente[] dipendenti;
     private int numeroDipendenti;
+
+    public GestoreDipendenti(int capacita) {
+        dipendenti = new Dipendente[capacita];
+        numeroDipendenti = 0;
+    }
+
+    // Metodo aggiungiDipendente(Dipendente d)
+    public void aggiungiDipendente(Dipendente d) {
+        if (numeroDipendenti < dipendenti.length) {
+            dipendenti[numeroDipendenti] = d;
+            numeroDipendenti++;
+        } else {
+            System.out.println("Capacità massima raggiunta. Impossibile aggiungere il dipendente.");
+        }
+    }
+
+    // Metodo calcolaStipendioTotale()
+    public double calcolaStipendioTotale() {
+        double totale = 0;
+        for (int i = 0; i < numeroDipendenti; i++) {
+            totale += dipendenti[i].calcolaStipendio();
+        }
+        return totale;
+    }
+
+    // Metodo stampaTuttiIDipendenti()
+    public void stampaTuttiIDipendenti() {
+        for (int i = 0; i < numeroDipendenti; i++) {
+            System.out.println(dipendenti[i]);
+        }
+    }
 }
